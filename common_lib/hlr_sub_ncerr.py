6ce335fd2c7f074fde2246689bac63b698b2ae63
Michael Reuter <reuterma@ornl.gov>
2006-03-24 02:34:30 +0000

    Stubbing BSS common functions. This refs #4.

---
+++ common_lib/hlr_sub_ncerr.py
@@ -0,0 +1,14 @@
+import array_manip
+import SOM.so
+import SOM.som
+
+def copy_attr(source,destination):
+    for key in source.attr_list.keys():
+        destination.attr_list[key]=source.attr_list[key]
+
+def sub_ncerr(left,right):
+    TITLE=SOM.som.SOM.TITLE
+    X_UNITS=SOM.som.SOM.X_UNITS
+    Y_UNITS=SOM.som.SOM.Y_UNITS
+
+    pass
---
+++ common_lib/hlr_wavelength_to_scalar_k.py
@@ -0,0 +1,14 @@
+import axis_manip
+import SOM.so
+import SOM.som
+
+def copy_attr(source,destination):
+    for key in source.attr_list.keys():
+        destination.attr_list[key]=source.attr_list[key]
+
+def wavelength_to_scalar_k(left,right):
+    TITLE=SOM.som.SOM.TITLE
+    X_UNITS=SOM.som.SOM.X_UNITS
+    Y_UNITS=SOM.som.SOM.Y_UNITS
+
+    pass
