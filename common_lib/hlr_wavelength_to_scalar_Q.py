009ad53cfb49daa1130431897e39ccc5e43da29b
Michael Reuter <reuterma@ornl.gov>
2006-03-24 21:31:04 +0000

    Adding documentation to more functions. This refs #4.

--- common_lib/hlr_wavelength_to_scalar_Q.py
+++ common_lib/hlr_wavelength_to_scalar_Q.py
@@ -3,10 +3,20 @@ import SOM.so
 import SOM.som
 
 def copy_attr(source,destination):
+    """
+    This function copies the attributes from the source SOM to the destination
+    SOM.
+
+    Parameters:
+    ----------
+    -> source is the SOM from which to copy the attributes
+    -> destination is the SOM that receives the copied attributes
+    """
+
     for key in source.attr_list.keys():
         destination.attr_list[key]=source.attr_list[key]
 
-def wavelength_to_scalar_Q(left,right):
+def wavelength_to_scalar_Q(obj):
     TITLE=SOM.som.SOM.TITLE
     X_UNITS=SOM.som.SOM.X_UNITS
     Y_UNITS=SOM.som.SOM.Y_UNITS
