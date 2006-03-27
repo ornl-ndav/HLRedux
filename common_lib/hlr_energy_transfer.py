f75c54a0e15c25f0eb75c389c964ecff29983be6
Michael Reuter <reuterma@ornl.gov>
2006-03-27 02:27:51 +0000

    Correcting function to appropriate operation. This refs #4.

--- common_lib/hlr_energy_transfer.py
+++ common_lib/hlr_energy_transfer.py
@@ -18,10 +18,10 @@ def copy_attr(source,destination):
 
 def energy_transfer(left,right):
     """
-    This function takes two SOMs, a SO and a SOM, two SOs, a tuple and a SOM,
-    a tuple and a SO, and two tuples and calculates the energy transfer in
-    units of THz. The SOM principle axes must be in units of meV. The SO and
-    tuples are assumed to be in units of meV.
+    This function takes a tuple and a SOM, a tuple and a SO or two tuples and
+    calculates the energy transfer in units of THz. The SOM principle axis
+    must be in units of meV. The SO and tuples are assumed to be in units of
+    meV.
 
     Parameters:
     ----------
@@ -34,69 +34,12 @@ def energy_transfer(left,right):
 
     Exceptions:
     ----------
-    <- IndexError is raised if the two SOMs do not contain the same number
-       of spectra
-    <- RuntimeError is raised if the x-axis units of the SOMs do not match
-    <- RuntimeError is raised if the x-axis units are not meV
-    <- RuntimeError is raised if the y-axis units of the SOMs do not match
-    <- RuntimeError is raised if the x-axes of the two SOs are not equal
 
+    <- RuntimeError is raised if the x-axis units are not meV
     """
 
     TITLE=SOM.som.SOM.TITLE
     X_UNITS=SOM.som.SOM.X_UNITS
-    Y_UNITS=SOM.som.SOM.Y_UNITS
-
-    def et_som_som(som1,som2):
-        # check that there is the same number of so
-        if len(som1)!=len(som2):
-            raise IndexError,"Can only add SOMs with same number of spectra"
-        #check that the units match up
-        if som1.attr_list[X_UNITS]!=som2.attr_list[X_UNITS]:
-            raise RuntimeError,"X units do not match"
-        if som1.attr_list[X_UNITS]!="meV":
-            raise RuntimeError,"X units must be meV"
-        if som1.attr_list[Y_UNITS]!=som2.attr_list[Y_UNITS]:
-            raise RuntimeError,"Y units do not match"
-
-        # create empty result som
-        result=SOM.som.SOM()
-
-        # attributes from som1 clobber those from som2
-        copy_attr(som2,result)
-        copy_attr(som1,result)
-
-        # do the calculation
-        for (so1,so2) in map(None,som1,som2):
-            result.append(et_so_so(so1,so2))
-
-        return result
-
-    def et_som_so(som,so):
-        # create empty result som
-        result=SOM.som.SOM()
-
-        # copy attributes from som
-        copy_attr(som,result)
-
-        # do the calculation
-        for it in som:
-            result.append(et_so_so(it,so))
-
-        return result
-
-    def et_so_som(so,som):
-        # create empty result som
-        result=SOM.som.SOM()
-
-        # copy attributes from som
-        copy_attr(som,result)
-
-        # do the calculation
-        for it in som:
-            result.append(et_so_so(so,it))
-
-        return result
 
     def et_som_num(som,num):
         # create empty result som
@@ -124,42 +67,43 @@ def energy_transfer(left,right):
 
         return result
 
-    def et_so_so(so1,so2):
-        if so1.x!=so2.x:
-            raise RunTimeError,"x axes must be equal"
-
-        # set up the result
-        result=SOM.so.SO()
-        result.id=so1.id
-        result.x=so1.x
-
-        # do the calculation
-        (result.y,result.var_y)=axis_manip.energy_transfer(so1.y,so1.var_y,
-                                                           so2.y,so2.var_y)
-
-        return result
-
     def et_so_num(so,num):
+        # BEGIN SNS-FIXME
+        import nessi_list
+        # dummy placeholder for x variance
+        # list is set to zero (I hope)
+        so_var_x=nessi_list.NessiList(len(so.x))
+        # END SNS-FIXME
+
         # set up the result
         result=SOM.so.SO()
         result.id=so.id
-        result.x=so.x
+        result.y=so.y
+        result.var_y=so.var_y
 
         # do the calculation
-        (result.y,result.var_y)=axis_manip.energy_transfer(so.y,so.var_y,
-                                                           num[0],num[1])
+        (result.x,var_x_throwaway)=axis_manip.energy_transfer(so.x,so_var_x,
+                                                              num[0],num[1])
 
         return result
 
     def et_num_so(num,so):
+        # BEGIN SNS-FIXME
+        import nessi_list
+        # dummy placeholder for x variance
+        # list is set to zero (I hope)
+        so_var_x=nessi_list.NessiList(len(so.x))
+        # END SNS-FIXME
+
         # set up the result
         result=SOM.so.SO()
         result.id=so.id
-        result.x=so.x
+        result.y=so.y
+        result.var_y=so.var_y
 
         # do the calculation
-        (result.y,result.var_y)=axis_manip.energy_transfer(num[0],num[1],
-                                                           so.y,so.var_y)
+        (result.x,var_x_throwaway)=axis_manip.energy_transfer(num[0],num[1],
+                                                              so.x,so_var_x)
 
         return result
 
@@ -173,11 +117,11 @@ def energy_transfer(left,right):
         left.attr_list[TITLE]
         try:
             right.attr_list[TITLE]
-            return et_som_som(left,right)
+            raise TypeError, "SOM-SOM operation not supported"
         except AttributeError:
             try:
                 right.id
-                return et_som_so(left,right)
+                raise TypeError, "SOM-SO operation not supported"
             except AttributeError:
                 return et_som_num(left,right)
     except AttributeError: # left is a so
@@ -188,11 +132,11 @@ def energy_transfer(left,right):
         left.id
         try:
             right.attr_list[TITLE]
-            return et_so_som(left,right)
+            raise TypeError, "SO-SOM operation not supported"
         except AttributeError:
             try:
                 right.id
-                return et_so_so(left,right)
+                raise TypeError, "SO-SO operation not supported"
             except AttributeError:
                 return et_so_num(left,right)
     except AttributeError: # left is a tuple
@@ -261,12 +205,8 @@ if __name__=="__main__":
     print "* scal-scal:",energy_transfer((2,1),(1,1))
     print "* so  -scal:",so_to_str(energy_transfer(som1[0],(1,1)))
     print "* scal-so  :",so_to_str(energy_transfer((1,1),som1[0]))
-    print "* so  -so  :",so_to_str(energy_transfer(som1[0],som1[1]))
     print "* som -scal:",energy_transfer(som1,(1,1))
     print "* scal-som :",energy_transfer((1,1),som1)
-    print "* som -so  :",energy_transfer(som1,som1[0])
-    print "* so  -som :",energy_transfer(som1[0],som1)
-    print "* som -som :",energy_transfer(som1,som2)
 
 
 
