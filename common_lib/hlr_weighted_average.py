169c4cf9f1b6e6ddbc2acccae2b08648a7c31df6
Michael Reuter <reuterma@ornl.gov>
2006-03-27 02:52:20 +0000

    Checkpointing weighted_average function. This refs #4.

--- common_lib/hlr_weighted_average.py
+++ common_lib/hlr_weighted_average.py
@@ -17,8 +17,52 @@ def copy_attr(source,destination):
         destination.attr_list[key]=source.attr_list[key]
 
 def weighted_average(obj,**kwargs):
-    TITLE=SOM.som.SOM.TITLE
-    X_UNITS=SOM.som.SOM.X_UNITS
-    Y_UNITS=SOM.som.SOM.Y_UNITS
+    """
+    This function takes a SOM or SO and calculates the weighted average for
+    the primary axis.
 
-    pass
+    Parameters:
+    ----------
+    ->
+    
+    Return:
+    ------
+    <-
+
+    Exceptions:
+    ----------
+    <- 
+    """
+    
+    TITLE=SOM.som.SOM.TITLE
+
+    def weighted_average_som(obj,**kwargs):
+
+        w_averages = []
+
+        for so in som:
+            w_averages.append(weighted_average_so(so,kwargs))
+
+        return w_averages
+
+    def weighted_average_so(obj,**kwargs):
+
+        
+
+    # determine if the obj is a som
+    try:
+        obj.attr_list[TITLE]
+        return weighted_average_som(obj)
+
+    except AttributeError:
+        pass
+
+    # determine if obj is a so
+    try:
+        obj.id
+        return weighted_average_so(obj)
+    except AttributeError:
+        raise TypeError, "Do not know how to handle given type"
+
+
+
