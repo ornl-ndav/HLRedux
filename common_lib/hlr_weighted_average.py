1c3ec411fc57fae2e07b43fa32acc6e474b43b50
Michael Reuter <reuterma@ornl.gov>
2006-03-27 14:43:46 +0000

    Completed filling out another function. This refs #4.

--- common_lib/hlr_weighted_average.py
+++ common_lib/hlr_weighted_average.py
@@ -23,46 +23,107 @@ def weighted_average(obj,**kwargs):
 
     Parameters:
     ----------
-    ->
+    -> obj is a SOM or SO that will have the weighted average calculated from
+       it
+    -> kwargs is a list of key word arguments that the function accepts:
+          start=<index of starting bin>
+          end=<index of ending bin>
     
     Return:
     ------
-    <-
+    <- A tuple (for a SO) or a list of tuples (for a SOM) containing the
+       weighted average and the uncertainty squared associated with the
+       weighted average
 
     Exceptions:
     ----------
-    <- 
+    <- TypeError is raised if a tuple or another construct (besides a SOM or
+       SO) is passed to the function
     """
     
     TITLE=SOM.som.SOM.TITLE
 
-    def weighted_average_som(obj,**kwargs):
+    if(kwargs.has_key("start")):
+        start=kwargs["start"]
+    else:
+        start=0
+        
+    def weighted_average_som(som,start,end):
 
         w_averages = []
 
         for so in som:
-            w_averages.append(weighted_average_so(so,kwargs))
+            w_averages.append(weighted_average_so(so,start,end))
 
         return w_averages
 
-    def weighted_average_so(obj,**kwargs):
+    def weighted_average_so(so,start,end):
 
-        
+        (w_ave, w_ave_err2)=utils.weighted_average(so.y, so.var_y, start, end)
+
+        return (w_ave,w_ave_err2)
 
     # determine if the obj is a som
     try:
         obj.attr_list[TITLE]
-        return weighted_average_som(obj)
-
+        if(kwargs.has_key("end")):
+            end=kwargs["end"]
+        else:
+            end=len(obj[0])-1
+            
+        return weighted_average_som(obj, start, end)
     except AttributeError:
         pass
 
     # determine if obj is a so
     try:
         obj.id
-        return weighted_average_so(obj)
+        if(kwargs.has_key("end")):
+            end=kwargs["end"]
+        else:
+            end=len(obj)-1
+
+        return weighted_average_so(obj, start, end)
     except AttributeError:
         raise TypeError, "Do not know how to handle given type"
 
+if __name__=="__main__":
+    def generate_so(start,stop=0):
+        if stop<start:
+            stop=start
+            start=0
+
+        so=SOM.so.SO()
+        if start==stop:
+            return so
+
+        so.x.extend(range(stop-start+1))
+        so.y.extend(range(start,stop))
+        so.var_y.extend(range(start,stop))
+        return so
+
+    def so_to_str(so):
+        if so==None:
+            return None
+        else:
+            return so.id,so.x,so.y,so.var_y
+
+    som1=SOM.som.SOM()
+    count=1
+    for i in range(2):
+        so=generate_so(count,count+5)
+        so.id=i+1
+        som1.append(so)
+        count+=5
+
+    print "********** SOM1"
+    print "* ",so_to_str(som1[0])
+    print "* ",so_to_str(som1[1])
+
+    print "********** weighted_average"
+    print "* rebin so       :",weighted_average(som1[0])
+    print "* rebin so [1,3] :",weighted_average(som1[0],start=1,end=3)
+    print "* rebin som      :",weighted_average(som1)
+    print "* rebin som [0,2]:",weighted_average(som1,start=0,end=2)
 
 
