1e7987a62ecba6e30ebc18c6a67fb5f3345bff2c
Pete Peterson <petersonpf@ornl.gov>
2006-04-11 17:41:43 +0000

    Added initial version of data helper utilities with all pass-throughs. Refs #14.

--- hlr_utils/__init__.py
+++ hlr_utils/__init__.py
@@ -1,6 +1,7 @@
 from hlr_1D_units import *
 from hlr_math_compatible import *
 from hlr_options import SNSOptions
+from hlr_data_helper import *
 
 #version
 __id__ = "$Id"
---
+++ hlr_utils/hlr_data_helper.py
@@ -0,0 +1,39 @@
+# this should be replaced with an enum
+SOM_type="SOM"
+SO_type="SO"
+num_type="number"
+
+def empty_result(arg1,arg2):
+    """
+    This function inspects the arguments and returns an appropriate
+    return type for an operation using the arguments. The object can
+    be filled using result_insert.
+    """
+
+    return (None,SOM_type)
+
+def result_insert(result,descr,value):
+    """
+    This function takes value and puts it into the result in an
+    appropriate fasion. The description is used for decision making.
+    """
+    pass
+
+def get_descr(obj):
+    return SOM_type
+
+def get_value(obj,index=0,descr=None):
+    """
+    This function takes an arbitrary object and returns the value for
+    the given index. If the object is not a collection the index is
+    ignored.
+    """
+    return 0
+
+def get_err2(obj,index,descr=None):
+    """
+    This function takes an arbitrary object and returns the
+    uncertainty squared for the given index. If the object is not a
+    collection the index is ignored.
+    """
+    return 0
