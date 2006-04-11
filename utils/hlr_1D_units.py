adf7e25cf22280518a1dfdfbab07eb27d5ab702b
Michael Reuter <reuterma@ornl.gov>
2006-04-11 00:47:11 +0000

    Adding helper functions. This refs #11.

---
+++ utils/hlr_1D_units.py
@@ -0,0 +1,33 @@
+def hlr_1D_units(som, units):
+    """
+    This function takes a SOM and a unit string and checks the SOMs primary
+    axes for the requested units. Raises an exception if units are not found.
+
+    Parameters:
+    ----------
+    -> som is the SOM to search the primary axes for units
+    -> units is the units to search the SOM primary axes for
+
+    Exceptions:
+    ----------
+    <- RuntimeError is raised if the requested units are not found
+    """
+    
+    if not som.hasAxisUnits(units):
+        raise RuntimeError,"No primary axis with %s units" % units
+
+def hlr_force_units(som, units, dim=1):
+    """
+    This function takse a SOM, a unit string and an optional dimension
+    parameter and replaces the units for the dimensional parameter minus 1 in
+    the SOM.
+
+    Parameters:
+    ----------
+    -> som is the SOM in which an axis units will be replaced
+    -> units is the units to replace the current one in the SOM
+    -> dim is the order of the primary axis. It's actual position in the SOM
+       list is dim-1
+    """
+    
+    som.setAxisUnits(units,dim-1)
---
+++ utils/hlr_math_compatible.py
@@ -0,0 +1,45 @@
+
+def hlr_math_compatible(som1, som2):
+    """
+    This function takes two SOMs and checks them for the following conditions:
+    1. The number of SOs in SOM1 is the same as SOM2
+    2. The units of the primary axes in SOM1 match those in SOM2
+    3. The y units of SOM1 match the y units in SOM2
+
+    If all three steps pass, nothing is done. If one step fails an exception
+    is raised.
+    
+    Parameters:
+    ----------
+    -> som1 is the first SOM to compare
+    -> som2 is the second SOM to compare
+
+    Exceptions:
+    ----------
+    <- IndexError is raised if the number of SOs in SOM1 and SOM2 are not equal
+    <- RuntimeError is raised if the units of the primary axes in SOM1 do not
+       match those in SOM2
+    <- RuntimeError is raised if the y units in SOM1 is not the same as in SOM2
+    """
+    
+    if len(som1)!=len(som2):
+        raise IndexError,"Can only add SOMs with same number of spectra"
+                
+    som1_axis_units = som1.getAllAxisUnits()
+    som2_axis_units = som2.getAllAxisUnits()
+
+    value = True
+    count = []
+    index = 1
+    for (unit1, unit2) in map(None, som1_axis_units, som2_axis_units):
+        if unit1 != unit2:
+            count.append(index)
+            value = False
+        index += 1
+            
+    if not value:
+        raise RuntimeError, "X units at %s do not match" % str(count)
+
+    if som1.getYUnits()!=som2.getYUnits():
+        raise RuntimeError,"Y units do not match"
+                
