37dd9c839a8a63a2cbdb9340f3f5d1a467bebce9
Pete Peterson <petersonpf@ornl.gov>
2006-04-11 17:52:14 +0000

    Added function get_length and small modification in get_descr. Refs #14.

--- hlr_utils/hlr_data_helper.py
+++ hlr_utils/hlr_data_helper.py
@@ -3,7 +3,7 @@ SOM_type="SOM"
 SO_type="SO"
 num_type="number"
 
-def empty_result(arg1,arg2):
+def empty_result(obj1,obj2):
     """
     This function inspects the arguments and returns an appropriate
     return type for an operation using the arguments. The object can
@@ -19,7 +19,14 @@ def result_insert(result,descr,value):
     """
     pass
 
-def get_descr(obj):
+def get_length(obj1,obj2=None):
+    """
+    This function returns the length appropriate for itterating
+    through the objects.
+    """
+    return 0
+
+def get_descr(obj1,obj2=None):
     return SOM_type
 
 def get_value(obj,index=0,descr=None):
