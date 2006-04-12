7eb09ac707b26469db76a56cafd9b89e3f556a22
Michael Reuter <reuterma@ornl.gov>
2006-04-12 14:45:27 +0000

    tof_to_wavelength HLR function now using object abstraction helpers. This refs #15.

--- common_lib/hlr_tof_to_wavelength.py
+++ common_lib/hlr_tof_to_wavelength.py
@@ -2,21 +2,7 @@ import axis_manip
 import SOM.so
 import SOM.som
 
-def copy_attr(source,destination):
-    """
-    This function copies the attributes from the source SOM to the destination
-    SOM.
-
-    Parameters:
-    ----------
-    -> source is the SOM from which to copy the attributes
-    -> destination is the SOM that receives the copied attributes
-    """
-
-    for key in source.attr_list.keys():
-        destination.attr_list[key]=source.attr_list[key]
-
-def tof_to_wavelength(obj):
+def tof_to_wavelength(obj,pathlength=None,units="microseconds"):
     """
     This function converts a primary axis of a SOM or SO from time-of-flight
     to wavelength. The wavelength axis for a SOM must be in units of
@@ -40,114 +26,49 @@ def tof_to_wavelength(obj):
     <- RuntimeError is raised if the SOM x-axis units are not microseconds
     """
 
-    TITLE=SOM.som.SOM.TITLE
-    X_UNITS=SOM.som.SOM.X_UNITS
+    # import the helper functions
+    import hlr_utils
 
-    def t2w_som(som):
-        if som.attr_list[X_UNITS]!="microseconds":
-            raise RuntimeError,"X units are not microseconds"
+    # set up for working through data
+    result,res_descr=hlr_utils.empty_result(obj)
+    o_descr,d_descr=hlr_utils.get_descr(obj)
 
-        # create empty result som
-        result=SOM.som.SOM()
+    # Primary axis for transformation. If a SO is passed, the function, will
+    # assume the axis for transformation is at the 0 position
+    if o_descr == "SOM":
+        axis = hlr_utils.hlr_1D_units(obj, units)
+    else:
+        axis = 0
 
-        copy_attr(som,result)
+    result=hlr_utils.copy_som_attr(result,res_descr,obj,o_descr)
 
-        for so in som:
-            result.append(t2w_so(so))
+    if pathlength == None:
+        pathlength=[20.0, 0.1]
 
-        return result
+    # iterate through the values
+    for i in range(hlr_utils.get_length(obj)):
+        val = hlr_utils.get_value(obj,i,o_descr,"x",axis)
+        err2 = hlr_utils.get_err2(obj,i,o_descr,"x",axis)
 
-    def t2w_so(so):
-        # BEGIN SNS-FIXME
-        import nessi_list
-        # dummy placeholder for x variance
-        # list is set to zero (I hope)
-        so_var_x=nessi_list.NessiList(len(so.x))
-        pathlength = [20.0, 0.1]
-        # END SNS-FIXME
+        value=axis_manip.tof_to_wavelength(val, err2,
+                                           pathlength[0], pathlength[1])
 
-        # set up the result
-        result=SOM.so.SO()
-        result.id=so.id
-        result.y=so.y
-        result.var_y=so.var_y
+        map_so = hlr_utils.get_map_so(obj,i)
+        hlr_utils.result_insert(result,res_descr,value,map_so,"x",axis)
 
-        (result.x,var_x_throwaway)=axis_manip.tof_to_wavelength(so.x,
-                                                                so_var_x,
-                                                                pathlength[0],
-                                                                pathlength[1])
+    return result
 
-        return result
-
-    def t2w_num(num):
-        # do the calculation
-        # BEGIN SNS-FIXME
-        pathlength = [20.0, 0.1]
-        # END SNS-FIXME
-        (wavelength, wavelength_err2)=axis_manip.tof_to_wavelength(num[0],
-                                                                   num[1],
-                                                                   pathlength[0],
-                                                                   pathlength[1])
-
-        return wavelength,wavelength_err2
-
-    # determine if the obj is a som
-    try:
-        obj.attr_list[TITLE]
-        return t2w_som(obj)
-
-    except AttributeError: # obj is a so
-        pass
-
-    # determine if obj is a so
-    try:
-        obj.id
-        return t2w_so(obj)
-
-    except AttributeError:
-        pass
-
-    # obj must be a tuple
-    return t2w_num(obj)
-
-    raise TypeError,"Do not know what to do with supplied types"
 
 if __name__=="__main__":
-    X_UNITS=SOM.som.SOM.X_UNITS
+    import hlr_test
 
-    def generate_so(start,stop=0):
-        if stop<start:
-            stop=start
-            start=0
-
-        so=SOM.so.SO()
-        if start==stop:
-            return so
-
-        so.x.extend(range(stop-start+1))
-        so.y.extend(range(start,stop))
-        so.var_y.extend(range(start,stop))
-        return so
-
-    def so_to_str(so):
-        if so==None:
-            return None
-        else:
-            return so.id,so.x,so.y,so.var_y
-
-    som1=SOM.som.SOM()
-    som1.attr_list[X_UNITS]="microseconds"
-    count=0
-    for i in range(2):
-        so=generate_so(count,count+5)
-        so.id=i+1
-        som1.append(so)
-        count+=5
+    som1=hlr_test.generate_som()
+    som1.setAllAxisUnits(["microseconds"])
 
     print "********** SOM1"
-    print "* ",so_to_str(som1[0])
-    print "* ",so_to_str(som1[1])
+    print "* ",som1[0]
+    print "* ",som1[1]
 
     print "********** tof_to_wavelength"
-    print "* rebin so :",so_to_str(tof_to_wavelength(som1[0]))
+    print "* rebin so :",tof_to_wavelength(som1[0])
     print "* rebin som:",tof_to_wavelength(som1)
