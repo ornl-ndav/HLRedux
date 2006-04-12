707f2617b172cce07bce97dc8a9eede37f654096
Michael Reuter <reuterma@ornl.gov>
2006-04-12 21:40:58 +0000

    Converting more functions. This refs #15.

--- common_lib/hlr_rebin_axis_2D.py
+++ common_lib/hlr_rebin_axis_2D.py
@@ -2,21 +2,7 @@ import axis_manip
 import SOM.so
 import SOM.som
 
-def copy_attr(source,destination):
-    """
-    This function copies the attributes from the source SOM to the destination
-    SOM.
-
-    Parameters:
-    ----------
-    -> source is the SOM from which to copy the attributes
-    -> destination is the SOM that receives the copied attributes
-    """
-
-    for key in source.attr_list.keys():
-        destination.attr_list[key]=source.attr_list[key]
-
-def rebin_axis_2D(obj,axis1,axis2):
+def rebin_axis_2D(obj,axis1_out,axis2_out):
     """
     This function rebins two primary axes for a SOM or a SO based on the
     given NessiList axis1 and axis2.
@@ -24,8 +10,10 @@ def rebin_axis_2D(obj,axis1,axis2):
     Parameters:
     ----------
     -> obj is the SOM or SO to be rebinned
-    -> axis1 is a NessiList containing the 1st axis to rebin the SOM or SO to
-    -> axis2 is a NessiList containing the 2nd axis to rebin the SOM or SO to
+    -> axis1_out is a NessiList containing the 1st axis to rebin the SOM or SO
+       to
+    -> axis2_out is a NessiList containing the 2nd axis to rebin the SOM or SO
+       to
 
     Returns:
     -------
@@ -36,94 +24,68 @@ def rebin_axis_2D(obj,axis1,axis2):
     <- TypeError is raised if the rebinning axis given is not a NessiList
     <- TypeError is raised if object being rebinned is not a SOM or a SO
     """
+    
+    # import the helper functions
+    import hlr_utils
 
-    TITLE=SOM.som.SOM.TITLE
-
-    def rebin_som(som, axis1, axis2):
-        # create empty result som
-        result=SOM.som.SOM()
-
-        copy_attr(som,result)
-
-        # do the rebinning
-        for so in som:
-            result.append(rebin_so(so,axis1,axis2))
-
-        return result
-
-    def rebin_so(so,axis1,axis2):
-        # set up the result
-        result=SOM.so.SO()
-        result.id=so.id
-        result.x=axis1
-
-        # do the rebinning
-        (result.y, result.var_y)=axis_manip.rebin_axis_2D(so.x,so.x,
-                                                          so.y,so.var_y,
-                                                          axis1,axis2)
-
-        return result
-
-    # Rebinning axis must be a NessiList. Fail now before getting too far.
+    # set up for working through data
     try:
-        axis1.__type__
-        axis2.__type__
+        axis1_out.__type__
     except AttributeError:
-        raise TypeError, "Axes must be NessiLists!"
+        raise TypeError, "Rebinning axis 1 must be a NessiList!"
 
-    # determine if the obj is a som
     try:
-        obj.attr_list[TITLE]
-        return rebin_som(obj,axis1,axis2)
-
+        axis2_out.__type__
     except AttributeError:
-        pass
+        raise TypeError, "Rebinning axis 2 must be a NessiList!"
+        
+    o_descr,d_descr=hlr_utils.get_descr(obj)
 
-    # determine if obj is a so
-    try:
-        obj.id
-        return rebin_so(obj,axis1,axis2)
-    except AttributeError:
+    if o_descr == "number":
         raise TypeError, "Do not know how to handle given type"
+    
+    result,res_descr=hlr_utils.empty_result(obj)
+
+    if res_descr == "SO":
+        print "00:",result.dim()
+
+    result=hlr_utils.copy_som_attr(result,res_descr,obj,o_descr)
+
+    # iterate through the values
+    for i in range(hlr_utils.get_length(obj)):
+        axis1_in = hlr_utils.get_value(obj,i,o_descr,"x",0)
+        axis2_in = hlr_utils.get_value(obj,i,o_descr,"x",1)
+        val = hlr_utils.get_value(obj,i,o_descr)
+        err2 = hlr_utils.get_err2(obj,i,o_descr)
+
+        value=axis_manip.rebin_axis_2D(axis1_in, axis2_in, val, err2,
+                                       axis1_out, axis2_out)
+        xvals=[]
+        xvals.append(axis1_out)
+        xvals.append(axis2_out)
+
+        map_so = hlr_utils.get_map_so(obj,None,i)
+        hlr_utils.result_insert(result,res_descr,value,map_so,"all",0,xvals)
+
+    return result
+
 
 if __name__=="__main__":
     import nessi_list
+    import hlr_test
 
-    def generate_so(start,stop=0):
-        if stop<start:
-            stop=start
-            start=0
+    som1=hlr_test.generate_som("histogram",2)
 
-        so=SOM.so.SO()
-        if start==stop:
-            return so
-
-        so.x.extend(range(stop-start+1))
-        so.y.extend(range(start,stop))
-        so.var_y.extend(range(start,stop))
-        return so
-
-    def so_to_str(so):
-        if so==None:
-            return None
-        else:
-            return so.id,so.x,so.y,so.var_y
-
-    som1=SOM.som.SOM()
-    count=0
-    for i in range(2):
-        so=generate_so(count,count+5)
-        so.id=i+1
-        som1.append(so)
-        count+=5
-
-    axis=nessi_list.NessiList()
-    axis.extend(0,2.5,5)
+    axis1=nessi_list.NessiList()
+    axis1.extend(0,2.5,5)
+    axis2=nessi_list.NessiList()
+    axis2.extend(0,2.5,5)
 
     print "********** SOM1"
-    print "* ",so_to_str(som1[0])
-    print "* ",so_to_str(som1[1])
+    print "* ",som1[0]
+    print "* ",som1[1]
 
-    print "********** rebin_axis_1D"
-    print "* rebin so :",so_to_str(rebin_axis_2D(som1[0],axis1,axis2))
+    print "********** rebin_axis_2D"
     print "* rebin som:",rebin_axis_2D(som1,axis1,axis2)
+    print "* rebin so :",rebin_axis_2D(som1[0],axis1,axis2)
+
--- common_lib/hlr_weighted_average.py
+++ common_lib/hlr_weighted_average.py
@@ -2,20 +2,6 @@ import utils
 import SOM.so
 import SOM.som
 
-def copy_attr(source,destination):
-    """
-    This function copies the attributes from the source SOM to the destination
-    SOM.
-
-    Parameters:
-    ----------
-    -> source is the SOM from which to copy the attributes
-    -> destination is the SOM that receives the copied attributes
-    """
-
-    for key in source.attr_list.keys():
-        destination.attr_list[key]=source.attr_list[key]
-
 def weighted_average(obj,**kwargs):
     """
     This function takes a SOM or SO and calculates the weighted average for
@@ -40,90 +26,54 @@ def weighted_average(obj,**kwargs):
     <- TypeError is raised if a tuple or another construct (besides a SOM or
        SO) is passed to the function
     """
-    
-    TITLE=SOM.som.SOM.TITLE
+
+    # import the helper functions
+    import hlr_utils
+
+    # set up for working through data
+    # This time highest object in the hierarchy is NOT what we need
+    result = []
+    res_descr = "number"
+    o_descr,d_descr=hlr_utils.get_descr(obj)
 
     if(kwargs.has_key("start")):
         start=kwargs["start"]
     else:
         start=0
-        
-    def weighted_average_som(som,start,end):
 
-        w_averages = []
-
-        for so in som:
-            w_averages.append(weighted_average_so(so,start,end))
-
-        return w_averages
-
-    def weighted_average_so(so,start,end):
-
-        (w_ave, w_ave_err2)=utils.weighted_average(so.y, so.var_y, start, end)
-
-        return (w_ave,w_ave_err2)
-
-    # determine if the obj is a som
-    try:
-        obj.attr_list[TITLE]
-        if(kwargs.has_key("end")):
-            end=kwargs["end"]
-        else:
-            end=len(obj[0])-1
+    if(kwargs.has_key("end")):
+        end=kwargs["end"]
+    else:
+        end=hlr_utils.get_length(obj)-1
             
-        return weighted_average_som(obj, start, end)
-    except AttributeError:
-        pass
+    result=hlr_utils.copy_som_attr(result,res_descr,obj,o_descr)
 
-    # determine if obj is a so
-    try:
-        obj.id
-        if(kwargs.has_key("end")):
-            end=kwargs["end"]
-        else:
-            end=len(obj)-1
+    # iterate through the values
+    for i in range(hlr_utils.get_length(obj)):
+        val = hlr_utils.get_value(obj,i,o_descr,"y")
+        err2 = hlr_utils.get_err2(obj,i,o_descr,"y")
 
-        return weighted_average_so(obj, start, end)
-    except AttributeError:
-        raise TypeError, "Do not know how to handle given type"
+        value=utils.weighted_average(val, err2, start, end)
+
+        hlr_utils.result_insert(result,res_descr,value,None,"all")
+
+    return result
+
 
 if __name__=="__main__":
-    def generate_so(start,stop=0):
-        if stop<start:
-            stop=start
-            start=0
+    import hlr_test
 
-        so=SOM.so.SO()
-        if start==stop:
-            return so
-
-        so.x.extend(range(stop-start+1))
-        so.y.extend(range(start,stop))
-        so.var_y.extend(range(start,stop))
-        return so
-
-    def so_to_str(so):
-        if so==None:
-            return None
-        else:
-            return so.id,so.x,so.y,so.var_y
-
-    som1=SOM.som.SOM()
-    count=1
-    for i in range(2):
-        so=generate_so(count,count+5)
-        so.id=i+1
-        som1.append(so)
-        count+=5
+    som1=hlr_test.generate_som()
 
     print "********** SOM1"
-    print "* ",so_to_str(som1[0])
-    print "* ",so_to_str(som1[1])
+    print "* ",som1[0]
+    print "* ",som1[1]
 
     print "********** weighted_average"
-    print "* rebin so       :",weighted_average(som1[0])
-    print "* rebin so [1,3] :",weighted_average(som1[0],start=1,end=3)
-    print "* rebin som      :",weighted_average(som1)
-    print "* rebin som [0,2]:",weighted_average(som1,start=0,end=2)
+    print "* som      :",weighted_average(som1)
+    print "* som [0,2]:",weighted_average(som1,start=0,end=2)
+    print "* so       :",weighted_average(som1[0])
+    print "* so  [1,3]:",weighted_average(som1[0],start=1,end=3)
+
 
 
