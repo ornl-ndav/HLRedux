3dc037fa91b5a130f32f745339a01017a99d8fba
Michael Reuter <reuterma@ornl.gov>
2006-06-02 01:11:55 +0000

    Removed missing addition.

--- hlr_utils/__init__.py
+++ hlr_utils/__init__.py
@@ -25,7 +25,6 @@ from hlr_math_compatible import *
 from hlr_options import SNSOptions
 from hlr_data_helper import *
 from hlr_driver_helper import *
-from hlr_config import Configure
 
 from version import version as __version__
 
