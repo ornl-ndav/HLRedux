0fa7bce112203ae04e3445c585e6865e8bda6546
Michael Reuter <reuterma@ornl.gov>
2006-06-16 15:25:33 +0000

    Adding configuration object. This resolves #8.

--- hlr_utils/hlr_config.py
+++ hlr_utils/hlr_config.py
@@ -20,14 +20,17 @@
 # its use would not infringe privately owned rights.
 #
 
-from hlr_1D_units import *
-from hlr_config import Configure
-from hlr_math_compatible import *
-from hlr_options import BasicOptions, SNSOptions
-from hlr_data_helper import *
-from hlr_driver_helper import *
+# $Id$
 
-from version import version as __version__
+class Configure:
+    """
+    This class provides a store location for various pieces of information
+    gathered during or necessary for the data reduction process. It can be
+    used later to provide metadata files, but that is handled in a separate
+    mechanism.
+    """
+    def __init__(self):
 
-#version
-__id__ = "$Id$"
+        self.verbose = None
+        self.data = None
+        self.output = None
