4af11b571f056b419760434ea5be641c11847a97
Michael Reuter <reuterma@ornl.gov>
2006-07-13 21:16:20 +0000

    Fixing PYLINT and other coding convention issues. This resolves #157.

--- common_lib/hlr_wavelength_to_scalar_k.py
+++ common_lib/hlr_wavelength_to_scalar_k.py
@@ -22,7 +22,7 @@
 
 # $Id$
 
-def wavelength_to_scalar_k(obj,**kwargs):
+def wavelength_to_scalar_k(obj, **kwargs):
     """
     This function converts a primary axis of a SOM or SO from wavelength
     to scalar_k. The wavelength axis for a SOM must be in units of Angstroms.
@@ -53,12 +53,12 @@ def wavelength_to_scalar_k(obj,**kwargs):
     import hlr_utils
 
     # set up for working through data
-    (result,res_descr)=hlr_utils.empty_result(obj)
-    (o_descr,d_descr)=hlr_utils.get_descr(obj)
+    (result, res_descr) = hlr_utils.empty_result(obj)
+    o_descr = hlr_utils.get_descr(obj)
 
     if o_descr == "list":
-        raise TypeError, "Do not know how to handle given type: %s" %\
-              o_descr
+        raise TypeError("Do not know how to handle given type: %s" % \
+                        o_descr)
     else:
         pass
 
@@ -75,7 +75,7 @@ def wavelength_to_scalar_k(obj,**kwargs):
     else:
         axis = 0
 
-    result=hlr_utils.copy_som_attr(result,res_descr,obj,o_descr)
+    result = hlr_utils.copy_som_attr(result, res_descr, obj, o_descr)
     if res_descr == "SOM":
         result = hlr_utils.hlr_force_units(result, "1/Angstroms", axis)
         result.setAxisLabel(axis, "scalar wavevector")
@@ -88,10 +88,10 @@ def wavelength_to_scalar_k(obj,**kwargs):
     import axis_manip
     
     for i in range(hlr_utils.get_length(obj)):
-        val = hlr_utils.get_value(obj,i,o_descr,"x",axis)
-        err2 = hlr_utils.get_err2(obj,i,o_descr,"x",axis)
+        val = hlr_utils.get_value(obj, i, o_descr, "x", axis)
+        err2 = hlr_utils.get_err2(obj, i, o_descr, "x", axis)
 
-        value=axis_manip.wavelength_to_scalar_k(val, err2)
+        value = axis_manip.wavelength_to_scalar_k(val, err2)
         if o_descr != "number":
             value1 = axis_manip.reverse_array_cp(value[0])
             value2 = axis_manip.reverse_array_cp(value[1])
@@ -100,37 +100,38 @@ def wavelength_to_scalar_k(obj,**kwargs):
             rev_value = value
             
         map_so = hlr_utils.get_map_so(obj,None,i)
-        if map_so != None:
-            map_so.y=axis_manip.reverse_array_cp(map_so.y)
-            map_so.var_y=axis_manip.reverse_array_cp(map_so.var_y)
+        if map_so is not None:
+            map_so.y = axis_manip.reverse_array_cp(map_so.y)
+            map_so.var_y = axis_manip.reverse_array_cp(map_so.var_y)
         else:
             pass
         
-        hlr_utils.result_insert(result,res_descr,rev_value,map_so,"x",axis)
+        hlr_utils.result_insert(result, res_descr, rev_value, map_so, "x",
+                                axis)
 
     return result
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     import hlr_test
 
-    som1=hlr_test.generate_som()
+    som1 = hlr_test.generate_som()
     som1.setAllAxisUnits(["Angstroms"])
 
-    som2=hlr_test.generate_som()
+    som2 = hlr_test.generate_som()
     som2.setAllAxisUnits(["Angstroms"])
 
     print "********** SOM1"
-    print "* ",som1[0]
-    print "* ",som1[1]
+    print "* ", som1[0]
+    print "* ", som1[1]
 
     print "********** SOM2"
-    print "* ",som2[0]
-    print "* ",som2[1]
+    print "* ", som2[0]
+    print "* ", som2[1]
 
     print "********** wavelength_to_scalar_k"
-    print "* som  :",wavelength_to_scalar_k(som1)
-    print "* so   :",wavelength_to_scalar_k(som2[0])
-    print "* scal :",wavelength_to_scalar_k((1,1))
+    print "* som  :", wavelength_to_scalar_k(som1)
+    print "* so   :", wavelength_to_scalar_k(som2[0])
+    print "* scal :", wavelength_to_scalar_k((1, 1))
 
 
