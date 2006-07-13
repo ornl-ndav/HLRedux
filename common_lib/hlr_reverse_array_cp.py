a278ff8066df4288bf6ed4cee908d5ef308251ff
Michael Reuter <reuterma@ornl.gov>
2006-07-13 20:07:13 +0000

    Fixing PYLINT and other coding convention issues. This resolves #150.

--- common_lib/hlr_reverse_array_cp.py
+++ common_lib/hlr_reverse_array_cp.py
@@ -47,41 +47,41 @@ def reverse_array_cp(obj):
     import hlr_utils
 
     # set up for working through data
-    (result,res_descr)=hlr_utils.empty_result(obj)
-    (o_descr,d_descr)=hlr_utils.get_descr(obj)
+    (result, res_descr) = hlr_utils.empty_result(obj)
+    o_descr = hlr_utils.get_descr(obj)
 
     if o_descr == "number" or o_descr == "list":
-        raise TypeError, "Do not know how to handle given type: %s" %\
-              o_descr
+        raise TypeError("Do not know how to handle given type: %s" % \
+                        o_descr)
     else:
         pass
 
-    result=hlr_utils.copy_som_attr(result,res_descr,obj,o_descr)
+    result = hlr_utils.copy_som_attr(result, res_descr, obj, o_descr)
 
     # iterate through the values
     import axis_manip
     
     for i in range(hlr_utils.get_length(obj)):
-        val = hlr_utils.get_value(obj,i,o_descr)
-        err2 = hlr_utils.get_err2(obj,i,o_descr)
+        val = hlr_utils.get_value(obj, i, o_descr)
+        err2 = hlr_utils.get_err2(obj, i, o_descr)
 
         value1 = axis_manip.reverse_array_cp(val)
         value2 = axis_manip.reverse_array_cp(err2)
 
-        map_so = hlr_utils.get_map_so(obj,None,i)
-        hlr_utils.result_insert(result,res_descr,(value1,value2),map_so)
+        map_so = hlr_utils.get_map_so(obj, None, i)
+        hlr_utils.result_insert(result, res_descr, (value1, value2), map_so)
 
     return result
 
-if __name__=="__main__":
+if __name__ == "__main__":
     import hlr_test
 
-    som1=hlr_test.generate_som()
+    som1 = hlr_test.generate_som()
 
     print "********** SOM1"
-    print "* ",som1[0]
-    print "* ",som1[1]
+    print "* ", som1[0]
+    print "* ", som1[1]
 
     print "********** reverse_array_cp"
-    print "* rev som:",reverse_array_cp(som1)
-    print "* rev so :",reverse_array_cp(som1[0])
+    print "* rev som:", reverse_array_cp(som1)
+    print "* rev so :", reverse_array_cp(som1[0])
