f2732e272bffc96d577287672dd8a0fe10605815
Michael Reuter <reuterma@ornl.gov>
2006-07-13 21:01:05 +0000

    Fixing PYLINT and other coding convention issues. This resolves #154.

--- common_lib/hlr_tof_to_initial_wavelength_igs.py
+++ common_lib/hlr_tof_to_initial_wavelength_igs.py
@@ -22,7 +22,7 @@
 
 # $Id$
 
-def tof_to_initial_wavelength_igs(obj,**kwargs):
+def tof_to_initial_wavelength_igs(obj, **kwargs):
     """
     This function converts a primary axis of a SOM or SO from time-of-flight
     to initial_wavelength_igs. The time-of-flight axis for a SOM must be in
@@ -66,8 +66,8 @@ def tof_to_initial_wavelength_igs(obj,**kwargs):
     import hlr_utils
 
     # set up for working through data
-    (result,res_descr)=hlr_utils.empty_result(obj)
-    (o_descr,d_descr)=hlr_utils.get_descr(obj)
+    (result, res_descr) = hlr_utils.empty_result(obj)
+    o_descr = hlr_utils.get_descr(obj)
 
     # Setup keyword arguments
     try:
@@ -107,7 +107,7 @@ def tof_to_initial_wavelength_igs(obj,**kwargs):
     else:
         axis = 0
 
-    result=hlr_utils.copy_som_attr(result,res_descr,obj,o_descr)
+    result = hlr_utils.copy_som_attr(result,res_descr,obj,o_descr)
     if res_descr == "SOM":
         result = hlr_utils.hlr_force_units(result, "Angstroms", axis)
         result.setAxisLabel(axis, "wavelength")
@@ -117,45 +117,47 @@ def tof_to_initial_wavelength_igs(obj,**kwargs):
         pass
 
     # Where to get instrument information
-    if dist_source_sample == None or dist_sample_detector == None:
+    if dist_source_sample is None or dist_sample_detector is None:
         if o_descr == "SOM":
             try:
                 obj.attr_list.instrument.get_primary()
                 inst = obj.attr_list.instrument
             except RuntimeError:
-                raise RuntimeError, "A detector was not provided!"
+                raise RuntimeError("A detector was not provided!")
         else:
-            if dist_source_sample == None and dist_sample_detector == None:
-                raise RuntimeError, "If a SOM is not passed, the "\
-                      +"source-sample and sample-detector distances must be "\
-                      +"provided."
-            elif dist_source_sample == None:
-                raise RuntimeError, "If a SOM is not passed, the "\
-                      +"source-sample distance must be provided."
-            elif dist_sample_detector == None:
-                raise RuntimeError, "If a SOM is not passed, the "\
-                  +"sample-detector distance must be provided."
+            if dist_source_sample is None and dist_sample_detector is None:
+                raise RuntimeError("If a SOM is not passed, the "\
+                                   +"source-sample and sample-detector "\
+                                   +"distances must be provided.")
+            elif dist_source_sample is None:
+                raise RuntimeError("If a SOM is not passed, the "\
+                                   +"source-sample distance must be provided.")
+            elif dist_sample_detector is None:
+                raise RuntimeError("If a SOM is not passed, the "\
+                                   +"sample-detector distance must be "\
+                                   +"provided.")
             else:
-                raise RuntimeError, "If you get here, see Steve Miller for "\
-                      +"your mug."
+                raise RuntimeError("If you get here, see Steve Miller for "\
+                                   +"your mug.")
     else:
         pass
         
-    if lambda_f != None:
-        (l_descr,e_descr) = hlr_utils.get_descr(lambda_f)
+    if lambda_f is not None:
+        l_descr = hlr_utils.get_descr(lambda_f)
     else:
         if o_descr == "SOM":
             try:
                 som_l_f = obj.attr_list["Wavelength_final"]
             except KeyError:
-                raise RuntimeError, "Please provide a final wavelength "\
-                      +"parameter either via the function call or the SOM"
+                raise RuntimeError("Please provide a final wavelength "\
+                                   +"parameter either via the function call "\
+                                   +"or the SOM")
         else:
-            raise RuntimeError, "You need to provide a final wavelength"
+            raise RuntimeError("You need to provide a final wavelength")
             
 
-    if time_zero != None:
-        (t_descr,e_descr) = hlr_utils.get_descr(time_zero)
+    if time_zero is not None:
+        t_descr = hlr_utils.get_descr(time_zero)
     else:
         if o_descr == "SOM":
             try:
@@ -163,123 +165,119 @@ def tof_to_initial_wavelength_igs(obj,**kwargs):
                 t_0 = obj.attr_list["Time_zero"][0]
                 t_0_err2 = obj.attr_list["Time_zero"][1]
             except KeyError:
-                raise RuntimeError, "Please provide a time-zero "\
-                      +"parameter either via the function call or the SOM"
+                raise RuntimeError("Please provide a time-zero "\
+                                   +"parameter either via the function call "\
+                                   +"or the SOM")
         else:
             t_0 = 0.0
             t_0_err2 = 0.0
 
 
-    if dist_source_sample != None:
-        (ls_descr,e_descr) = hlr_utils.get_descr(dist_source_sample)
+    if dist_source_sample is not None:
+        ls_descr = hlr_utils.get_descr(dist_source_sample)
     # Do nothing, go on
     else:
         pass
 
-    if dist_sample_detector != None:
-        (ld_descr,e_descr) = hlr_utils.get_descr(dist_sample_detector)
+    if dist_sample_detector is not None:
+        ld_descr = hlr_utils.get_descr(dist_sample_detector)
     # Do nothing, go on
     else:
         pass
-
-    if o_descr == "SOM" and run_filter:
-        length = len(obj[0])
-        filter = True
-    elif o_descr == "SO" and run_filter:
-        length = len(obj)
-        filter = True
-    else:
-        filter = False
 
     # iterate through the values
     import axis_manip
     
     for i in range(hlr_utils.get_length(obj)):
-        val = hlr_utils.get_value(obj,i,o_descr,"x",axis)
-        err2 = hlr_utils.get_err2(obj,i,o_descr,"x",axis)
+        val = hlr_utils.get_value(obj, i, o_descr, "x", axis)
+        err2 = hlr_utils.get_err2(obj, i, o_descr, "x", axis)
 
-        map_so = hlr_utils.get_map_so(obj,None,i)
+        map_so = hlr_utils.get_map_so(obj, None, i)
 
-        if dist_source_sample == None:
-            (L_s,L_s_err2) = hlr_utils.get_parameter("primary",map_so,inst)
+        if dist_source_sample is None:
+            (L_s, L_s_err2) = hlr_utils.get_parameter("primary", map_so, inst)
         else:
-            L_s = hlr_utils.get_value(dist_source_sample,i,ls_descr)
-            L_s_err2 = hlr_utils.get_err2(dist_source_sample,i,ls_descr)
+            L_s = hlr_utils.get_value(dist_source_sample, i, ls_descr)
+            L_s_err2 = hlr_utils.get_err2(dist_source_sample, i, ls_descr)
 
-        if dist_sample_detector == None:
-            (L_d,L_d_err2) = hlr_utils.get_parameter("secondary",map_so,inst)
+        if dist_sample_detector is None:
+            (L_d, L_d_err2) = hlr_utils.get_parameter("secondary", map_so,
+                                                      inst)
         else:
-            L_d = hlr_utils.get_value(dist_sample_detector,i,ld_descr)
-            L_d_err2 = hlr_utils.get_err2(dist_sample_detector,i,ld_descr)
+            L_d = hlr_utils.get_value(dist_sample_detector, i, ld_descr)
+            L_d_err2 = hlr_utils.get_err2(dist_sample_detector, i, ld_descr)
 
-        if lambda_f != None:
-            l_f = hlr_utils.get_value(lambda_f,i,t_descr)
-            l_f_err2 = hlr_utils.get_err2(lambda_f,i,t_descr)
+        if lambda_f is not None:
+            l_f = hlr_utils.get_value(lambda_f, i, t_descr)
+            l_f_err2 = hlr_utils.get_err2(lambda_f, i, t_descr)
         else:
             l_f_tuple = hlr_utils.get_special(som_l_f, map_so)
             l_f = l_f_tuple[0]
             l_f_err2 = l_f_tuple[1]
             
-        if time_zero != None:
-            t_0 = hlr_utils.get_value(time_zero,i,t_descr)
-            t_0_err2 = hlr_utils.get_err2(time_zero,i,t_descr)
+        if time_zero is not None:
+            t_0 = hlr_utils.get_value(time_zero, i, t_descr)
+            t_0_err2 = hlr_utils.get_err2(time_zero, i, t_descr)
         else:
             pass
 
-        value=axis_manip.tof_to_initial_wavelength_igs(val, err2,
-                                                       l_f, l_f_err2,
-                                                       t_0, t_0_err2,
-                                                       L_s, L_s_err2,
-                                                       L_d, L_d_err2)
+        value = axis_manip.tof_to_initial_wavelength_igs(val, err2,
+                                                         l_f, l_f_err2,
+                                                         t_0, t_0_err2,
+                                                         L_s, L_s_err2,
+                                                         L_d, L_d_err2)
 
         # Remove all wavelengths < 0
-        if filter:
+        if run_filter:
             index = 0
             for val in value[0]:
                 if val > 0:
                     break
                 index += 1
 
-            value[0].__delslice__(0,index)
-            value[1].__delslice__(0,index)
-            map_so.y.__delslice__(0,index)
-            map_so.var_y.__delslice__(0,index)
+            value[0].__delslice__(0, index)
+            value[1].__delslice__(0, index)
+            map_so.y.__delslice__(0, index)
+            map_so.var_y.__delslice__(0, index)
+        else:
+            pass
 
-        hlr_utils.result_insert(result,res_descr,value,map_so,"x",axis)
+        hlr_utils.result_insert(result, res_descr, value, map_so, "x", axis)
 
     return result
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     import hlr_test
     import SOM
 
-    l_f = (7.0, 0.1)    
-    t_0 = (0.1, 0.001)
+    l_f_i = (7.0, 0.1)    
+    t_0_i = (0.1, 0.001)
     d_ss = (15.0, 0.1)
     d_sd = (1.0, 0.05)
 
-    som1=hlr_test.generate_som()
+    som1 = hlr_test.generate_som()
     som1.setAllAxisUnits(["microseconds"])
-    som1.attr_list["Wavelength_final"] = l_f
-    som1.attr_list["Time_zero"] = t_0
+    som1.attr_list["Wavelength_final"] = l_f_i
+    som1.attr_list["Time_zero"] = t_0_i
     som1.attr_list.instrument = SOM.ASG_Instrument()
     
     print "********** SOM1"
-    print "* ",som1[0]
-    print "* ",som1[1]
+    print "* ", som1[0]
+    print "* ", som1[1]
 
     print "********** tof_to_initial_wavelength_igs"
-    print "* som  :",tof_to_initial_wavelength_igs(som1,run_filter=False)
-    print "* so   :",tof_to_initial_wavelength_igs(som1[0],lambda_f=l_f,
-                                                   time_zero=t_0,
-                                                   dist_source_sample=d_ss,
-                                                   dist_sample_detector=d_sd,
-                                                   run_filter=False)
-    print "* scal :",tof_to_initial_wavelength_igs([1,1],lambda_f=l_f,
-                                                   time_zero=t_0,
-                                                   dist_source_sample=d_ss,
-                                                   dist_sample_detector=d_sd)
+    print "* som  :", tof_to_initial_wavelength_igs(som1, run_filter=False)
+    print "* so   :", tof_to_initial_wavelength_igs(som1[0], lambda_f=l_f_i,
+                                                    time_zero=t_0_i,
+                                                    dist_source_sample=d_ss,
+                                                    dist_sample_detector=d_sd,
+                                                    run_filter=False)
+    print "* scal :", tof_to_initial_wavelength_igs([1, 1], lambda_f=l_f_i,
+                                                    time_zero=t_0_i,
+                                                    dist_source_sample=d_ss,
+                                                    dist_sample_detector=d_sd,
+                                                    run_filter=False)
 
 
 
