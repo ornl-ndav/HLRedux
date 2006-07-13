dcfad031d5f907a400d4659737e261c4d7fdc434
Michael Reuter <reuterma@ornl.gov>
2006-07-13 19:56:48 +0000

    Fixing PYLINT and other coding convention issues. This resolves #146.

--- common_lib/hlr_init_scatt_wavevector_to_scalar_Q.py
+++ common_lib/hlr_init_scatt_wavevector_to_scalar_Q.py
@@ -22,7 +22,7 @@
 
 # $Id$
 
-def init_scatt_wavevector_to_scalar_Q(initk,scattk,**kwargs):
+def init_scatt_wavevector_to_scalar_Q(initk, scattk, **kwargs):
     """
     This function takes an initial wavevector and a scattered wavevector as a
     tuple and a SOM, a tuple and a SO  or two tuples and calculates the
@@ -63,18 +63,18 @@ def init_scatt_wavevector_to_scalar_Q(initk,scattk,**kwargs):
     import hlr_utils
 
     # set up for working through data
-    (result,res_descr)=hlr_utils.empty_result(initk,scattk)
-    (i_descr,s_descr)=hlr_utils.get_descr(initk,scattk)
+    (result, res_descr) = hlr_utils.empty_result(initk, scattk)
+    (i_descr, s_descr) = hlr_utils.get_descr(initk, scattk)
 
     # error checking for types
     if i_descr == "SOM" and s_descr == "SOM":
-        raise TypeError, "SOM-SOM operation not supported"
+        raise TypeError("SOM-SOM operation not supported")
     elif i_descr == "SOM" and s_descr == "SO":
-        raise TypeError, "SOM-SO operation not supported"
+        raise TypeError("SOM-SO operation not supported")
     elif i_descr == "SO" and s_descr == "SOM":
-        raise TypeError, "SO-SOM operation not supported"
+        raise TypeError("SO-SOM operation not supported")
     elif i_descr == "SO" and s_descr == "SO":
-        raise TypeError, "SO-SO operation not supported"
+        raise TypeError("SO-SO operation not supported")
     else:
         pass
 
@@ -89,8 +89,8 @@ def init_scatt_wavevector_to_scalar_Q(initk,scattk,**kwargs):
     except KeyError:
         units = "1/Angstroms"
 
-    result=hlr_utils.copy_som_attr(result,res_descr,initk,i_descr,
-                                   scattk,s_descr)
+    result = hlr_utils.copy_som_attr(result, res_descr, initk, i_descr,
+                                     scattk, s_descr)
     if res_descr == "SOM":
         index = hlr_utils.hlr_1D_units(result, units)
         result = hlr_utils.hlr_force_units(result, units, index)
@@ -106,70 +106,71 @@ def init_scatt_wavevector_to_scalar_Q(initk,scattk,**kwargs):
                 initk.attr_list.instrument.get_primary()
                 inst = initk.attr_list.instrument
             except RuntimeError:
-                raise RuntimeError, "A detector was not provided!"
+                raise RuntimeError("A detector was not provided!")
 
         elif s_descr == "SOM":
             try:
                 scattk.attr_list.instrument.get_primary()
                 inst = scattk.attr_list.instrument
             except RuntimeError:
-                raise RuntimeError, "A detector was not provided!"
+                raise RuntimeError("A detector was not provided!")
 
         else:
-            raise RuntimeError, "If no SOM is provided, then polar "\
-                  +"information must be given."
+            raise RuntimeError("If no SOM is provided, then polar "\
+                               +"information must be given.")
     else:
-        (p_descr,e_descr) = hlr_utils.get_descr(polar)
+        p_descr = hlr_utils.get_descr(polar)
 
     # iterate through the values
     import axis_manip
     
-    for i in range(hlr_utils.get_length(initk,scattk)):
-        val1 = hlr_utils.get_value(initk,i,i_descr,"x")
-        err2_1 = hlr_utils.get_err2(initk,i,i_descr,"x")
+    for i in range(hlr_utils.get_length(initk, scattk)):
+        val1 = hlr_utils.get_value(initk, i, i_descr, "x")
+        err2_1 = hlr_utils.get_err2(initk, i, i_descr, "x")
         
-        val2 = hlr_utils.get_value(scattk,i,s_descr,"x")
-        err2_2 = hlr_utils.get_err2(scattk,i,s_descr,"x")
+        val2 = hlr_utils.get_value(scattk, i, s_descr, "x")
+        err2_2 = hlr_utils.get_err2(scattk, i, s_descr, "x")
 
-        map_so = hlr_utils.get_map_so(initk,scattk,i)
+        map_so = hlr_utils.get_map_so(initk, scattk, i)
 
-        if polar == None:
-            (angle,angle_err2) = hlr_utils.get_parameter("polar",map_so,inst)
+        if polar is None:
+            (angle, angle_err2) = hlr_utils.get_parameter("polar", map_so,
+                                                          inst)
         else:
-            angle = hlr_utils.get_value(polar,i,p_descr)
-            angle_err2 = hlr_utils.get_err2(polar,i,p_descr)
+            angle = hlr_utils.get_value(polar, i, p_descr)
+            angle_err2 = hlr_utils.get_err2(polar, i, p_descr)
             
-        value=axis_manip.init_scatt_wavevector_to_scalar_Q(val1, err2_1,
-                                                           val2, err2_2,
-                                                           angle, angle_err2)
+        value = axis_manip.init_scatt_wavevector_to_scalar_Q(val1, err2_1,
+                                                             val2, err2_2,
+                                                             angle, angle_err2)
 
-        hlr_utils.result_insert(result,res_descr,value,map_so,"x")
+        hlr_utils.result_insert(result, res_descr, value, map_so, "x")
 
     return result
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     import hlr_test
     import SOM
 
-    som1=hlr_test.generate_som()
+    som1 = hlr_test.generate_som()
     som1.setAllAxisUnits(["1/Angstroms"])
     som1.attr_list.instrument = SOM.ASG_Instrument()
-    pa = (0.785,0.005)
+    pa = (0.785, 0.005)
 
     print "********** SOM1"
-    print "* ",som1[0]
-    print "* ",som1[1]
+    print "* ", som1[0]
+    print "* ", som1[1]
 
     print "********** init_scatt_wavevector_to_scalar_Q"
-    print "* som -scal:",init_scatt_wavevector_to_scalar_Q(som1,(1,1))
-    print "* scal-som :",init_scatt_wavevector_to_scalar_Q((1,1),som1)
-    print "* so  -scal:",init_scatt_wavevector_to_scalar_Q(som1[0],(1,1),
-                                                           polar=pa)
-    print "* scal-so  :",init_scatt_wavevector_to_scalar_Q((1,1),som1[0],
-                                                           polar=pa)
-    print "* scal-scal:",init_scatt_wavevector_to_scalar_Q((2,1),(1,1),
-                                                           polar=pa)
+    print "* som -scal:", init_scatt_wavevector_to_scalar_Q(som1, (1, 1))
+    print "* scal-som :", init_scatt_wavevector_to_scalar_Q((1, 1), som1)
+    print "* so  -scal:", init_scatt_wavevector_to_scalar_Q(som1[0], (1, 1),
+                                                            polar=pa)
+    print "* scal-so  :", init_scatt_wavevector_to_scalar_Q((1, 1), som1[0],
+                                                            polar=pa)
+    print "* scal-scal:", init_scatt_wavevector_to_scalar_Q((2, 1), (1, 1),
+                                                            polar=pa)
 
 
 
