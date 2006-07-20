b71ce59d64b8722e53e1e93a78aa55e2f761ac0d
Michael Reuter <reuterma@ornl.gov>
2006-07-20 18:49:30 +0000

    Fixed PYLINT and coding convention issues. This resolves #176.

--- dr_lib/hlr_rebin_efficiency.py
+++ dr_lib/hlr_rebin_efficiency.py
@@ -22,7 +22,7 @@
 
 # $Id$
 
-def rebin_efficiency(obj1,obj2,**kwargs):
+def rebin_efficiency(obj1, obj2, **kwargs):
     """
     This function takes two SOMs or two SOs and rebins the data for obj1 onto
     the axis provided by obj2. The units on the x-axes needs to be Angstroms,
@@ -55,14 +55,14 @@ def rebin_efficiency(obj1,obj2,**kwargs):
     import hlr_utils
     
     # set up for working through data
-    (result,res_descr)=hlr_utils.empty_result(obj1,obj2)
-    (o1_descr,o2_descr)=hlr_utils.get_descr(obj1,obj2)
+    (result, res_descr) = hlr_utils.empty_result(obj1, obj2)
+    (o1_descr, o2_descr) = hlr_utils.get_descr(obj1, obj2)
     
     # error checking for types
     if o1_descr == "SOM" and o2_descr == "SO":
         raise TypeError, "SOM-SO operation not supported"
     elif o1_descr == "SO" and o2_descr == "SOM":
-        raise TypeError, "SO-SOM operation not supported"
+        raise TypeError("SO-SOM operation not supported")
     # Have the right object combination, go on
     else:
         pass
@@ -74,15 +74,15 @@ def rebin_efficiency(obj1,obj2,**kwargs):
         units = "Angstroms"
    
     if o1_descr == "SOM" and o2_descr == "SOM":
-         hlr_utils.hlr_math_compatible(obj1,o1_descr,obj2,o2_descr)
+        hlr_utils.hlr_math_compatible(obj1, o1_descr, obj2, o2_descr)
     # If both objects are not SOMs, do nothing
     else:
         pass
 
-    result=hlr_utils.copy_som_attr(result,res_descr,obj2,o2_descr)
+    result = hlr_utils.copy_som_attr( result, res_descr, obj2, o2_descr)
 
     if res_descr == "SOM":
-        result = hlr_utils.hlr_force_units(result, "Angstroms")
+        result = hlr_utils.hlr_force_units(result, units)
     # Can't force units on anything other than a SOM
     else:
         pass
@@ -90,50 +90,50 @@ def rebin_efficiency(obj1,obj2,**kwargs):
     # iterate through the values
     import common_lib
 
-    for i in range(hlr_utils.get_length(obj1,obj2)):
-        val1 = hlr_utils.get_value(obj1,i,o1_descr,"all")
-        val2 = hlr_utils.get_value(obj2,i,o2_descr,"x")
+    for i in range(hlr_utils.get_length(obj1, obj2)):
+        val1 = hlr_utils.get_value(obj1, i, o1_descr, "all")
+        val2 = hlr_utils.get_value(obj2, i, o2_descr, "x")
         
-        value=common_lib.rebin_axis_1D(val1, val2)
+        value = common_lib.rebin_axis_1D(val1, val2)
         
-        hlr_utils.result_insert(result,res_descr,value,None,"all")
+        hlr_utils.result_insert(result, res_descr, value, None, "all")
 
     return result
                                                     
     
-if __name__=="__main__":
+if __name__ == "__main__":
     import hlr_test
     import SOM
 
-    som1=SOM.SOM()
+    som1 = SOM.SOM()
     som1.setAllAxisUnits(["Angstroms"])
-    so1=SOM.SO()    
-    so1.id=1
-    so1.axis[0].val.extend(range(0,7,2))
-    so1.y.extend(0.994,0.943,0.932)
-    so1.var_y.extend(0.010,0.012,0.013)
+    so1 = SOM.SO()    
+    so1.id = 1
+    so1.axis[0].val.extend(range(0, 7, 2))
+    so1.y.extend(0.994, 0.943, 0.932)
+    so1.var_y.extend(0.010, 0.012, 0.013)
     som1.append(so1)
-    so2=SOM.SO()    
-    so2.id=2
-    so2.axis[0].val.extend(range(0,7,2))
-    so2.y.extend(0.934,0.986,0.957)
-    so2.var_y.extend(0.011,0.010,0.015)
+    so2 = SOM.SO()    
+    so2.id = 2
+    so2.axis[0].val.extend(range(0, 7, 2))
+    so2.y.extend(0.934, 0.986, 0.957)
+    so2.var_y.extend(0.011, 0.010, 0.015)
     som1.append(so2)
 
-    som2=hlr_test.generate_som()
+    som2 = hlr_test.generate_som()
     som2.setAllAxisUnits(["Angstroms"])
 
     
     print "********** SOM1"
-    print "* ",som1[0]
-    print "* ",som1[1]
+    print "* ", som1[0]
+    print "* ", som1[1]
 
     print "********** SOM2"
-    print "* ",som2[0]
-    print "* ",som2[1]
+    print "* ", som2[0]
+    print "* ", som2[1]
 
     print "********** rebin_efficiency"
-    print "* som+som :",rebin_efficiency(som1,som2)
-    print "* so +so  :",rebin_efficiency(som1[0],som2[0])
+    print "* som+som :", rebin_efficiency(som1, som2)
+    print "* so +so  :", rebin_efficiency(som1[0], som2[0])
 
     
