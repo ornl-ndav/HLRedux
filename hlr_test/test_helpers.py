2009ee94a760d8f30776259bcdffc84e58187d55
Michael Reuter <reuterma@ornl.gov>
2006-07-20 19:00:23 +0000

    Fixed PYLINT and coding convention issues. This resolves #179.

--- hlr_test/test_helpers.py
+++ hlr_test/test_helpers.py
@@ -24,14 +24,14 @@
 
 import SOM
 
-def generate_so(type,start,stop=0,dim=1,extra=0):
+def generate_so(data_type, start, stop=0, dim=1, extra=0):
     """
     This function generates a SO for testing purposes. The SO can be either
     a histogram or density.
 
     Parameters:
     ----------
-    -> type is defined as histogram or density
+    -> data_type is defined as histogram or density
     -> start is the starting value for number generation
     -> stop (OPTIONAL) is the stopping value for number generation
     -> dim (OPTIONAL) allows for making SOs multi-dimensional
@@ -41,41 +41,41 @@ def generate_so(type,start,stop=0,dim=1,extra=0):
     <- A SO
     """
     
-    if stop<start:
-        stop=start
-        start=0
+    if stop < start:
+        stop = start
+        start = 0
         
-    so=SOM.SO(dim)
-    if start==stop:
+    so = SOM.SO(dim)
+    if start == stop:
         return so
 
-    if type.lower() == "histogram":
-        num = stop-start+1
+    if data_type.lower() == "histogram":
+        num = stop - start + 1
     else:
-        num = stop-start
+        num = stop - start
 
     for i in range(dim):
         so.axis[i].val.extend(range(num))
         size = len(so.axis[i].val)
 
         if i == 0:
-            so.y.extend(range(start+extra,stop+extra))
-            so.var_y.extend(range(start+extra,stop+extra))
+            so.y.extend(range(start + extra, stop + extra))
+            so.var_y.extend(range(start + extra, stop + extra))
         else:
             for j in range(size - 2):
-                so.y.extend(range(start+extra,stop+extra))
-                so.var_y.extend(range(start+extra,stop+extra))
+                so.y.extend(range(start + extra, stop + extra))
+                so.var_y.extend(range(start + extra, stop + extra))
 
     return so
 
 
-def generate_som(type="histogram",dim=1,number=2):
+def generate_som(data_type="histogram", dim=1, number=2):
     """
     This function generates a SOM for testing purposes.
 
     Parameters:
     ----------
-    -> type is defined as histogram or density
+    -> data_type is defined as histogram or density
 
     Returns:
     -------
@@ -83,11 +83,11 @@ def generate_som(type="histogram",dim=1,number=2):
     """
 
     som = SOM.SOM()
-    count=0
+    count = 0
     for i in range(number):
-        so=generate_so(type,count,count+5,dim)
-        so.id=i+1
+        so = generate_so(data_type, count, count + 5, dim)
+        so.id = i + 1
         som.append(so)
-        count+=5
+        count += 5
 
     return som
