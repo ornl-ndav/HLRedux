cf46b3f0500e4ffd75f0e930b33e4c4cb1695688
Michael Reuter <reuterma@ornl.gov>
2006-07-20 16:09:25 +0000

    Fixed PYLINT and coding convention issues. This refs #173.

--- dr_lib/hlr_create_E_vs_Q.py
+++ dr_lib/hlr_create_E_vs_Q.py
@@ -22,7 +22,7 @@
 
 # $Id$
 
-def create_E_vs_Q(obj,E,k,px_separate=True,*args,**kwargs):
+def create_E_vs_Q(obj, E, k):
     """
     This functions takes a SOM with a wavelength axis, energy and wavevector
     tuples and turns the SOs contained in the SOM to 2D SOs with E and Q axes.
@@ -47,11 +47,11 @@ def create_E_vs_Q(obj,E,k,px_separate=True,*args,**kwargs):
     import hlr_utils
 
     # set up for working through data
-    (result,res_descr)=hlr_utils.empty_result(obj)
-    o_descr=hlr_utils.get_descr(obj)
+    (result, res_descr) = hlr_utils.empty_result(obj)
+    o_descr = hlr_utils.get_descr(obj)
 
     if o_descr != "SOM":
-        raise RuntimeError, "This function only accepts SOMs"
+        raise RuntimeError("This function only accepts SOMs")
     # Have a SOM, go on
     else:
         pass
@@ -61,18 +61,18 @@ def create_E_vs_Q(obj,E,k,px_separate=True,*args,**kwargs):
             obj.attr_list.instrument.get_primary()
             inst = obj.attr_list.instrument
         except RuntimeError:
-            raise RuntimeError, "An instrument was not provided!"
+            raise RuntimeError("An instrument was not provided!")
     # Got the instrument, go on
     else:
         pass
 
     axis = hlr_utils.hlr_1D_units(obj, "Angstroms")
 
-    result=hlr_utils.copy_som_attr(result,res_descr,obj,o_descr)
+    result = hlr_utils.copy_som_attr(result, res_descr, obj, o_descr)
     result.setYLabel("counts")
     result.setYUnits("counts / (ueV * A^-1)")
-    result.setAllAxisLabels(["Q transfer","energy transfer"])
-    result.setAllAxisUnits(["A^-1","ueV"])
+    result.setAllAxisLabels(["Q transfer", "energy transfer"])
+    result.setAllAxisUnits(["A^-1", "ueV"])
 
     # iterate through the values
     import array_manip
@@ -85,28 +85,28 @@ def create_E_vs_Q(obj,E,k,px_separate=True,*args,**kwargs):
 
     for j in range(hlr_utils.get_length(obj)):
 
-        xval = hlr_utils.get_value(obj,j,o_descr,"x",axis)
-        xerr2 = hlr_utils.get_err2(obj,j,o_descr,"x",axis)
+        xval = hlr_utils.get_value(obj, j, o_descr, "x", axis)
+        xerr2 = hlr_utils.get_err2(obj, j, o_descr, "x", axis)
 
-        k_j=axis_manip.wavelength_to_scalar_k(xval, xerr2)
-        E_j=axis_manip.wavelength_to_energy(xval, xerr2)
+        k_j = axis_manip.wavelength_to_scalar_k(xval, xerr2)
+        E_j = axis_manip.wavelength_to_energy(xval, xerr2)
 
-        yval = hlr_utils.get_value(obj,j,o_descr,"y")
-        yerr2 = hlr_utils.get_err2(obj,j,o_descr,"y")
+        yval = hlr_utils.get_value(obj, j, o_descr, "y")
+        yerr2 = hlr_utils.get_err2(obj, j, o_descr, "y")
 
         yval = axis_manip.reverse_array_cp(yval)
         yerr2 = axis_manip.reverse_array_cp(yerr2)
 
-        so = hlr_utils.get_value(obj,j,o_descr,"all")
-        (angle,angle_err2) = hlr_utils.get_parameter("polar",so,inst)
+        so = hlr_utils.get_value(obj, j, o_descr, "all")
+        (angle, angle_err2) = hlr_utils.get_parameter("polar", so, inst)
 
-        E_t=axis_manip.energy_transfer(E_j[0], E_j[1], E[0], E[1])
-        E_t=axis_manip.frequency_to_energy(E_t[0], E_t[1])
-        E_t=array_manip.mult_ncerr(E_t[0], E_t[1], 1000.0, 0.0)
+        E_t = axis_manip.energy_transfer(E_j[0], E_j[1], E[0], E[1])
+        E_t = axis_manip.frequency_to_energy(E_t[0], E_t[1])
+        E_t = array_manip.mult_ncerr(E_t[0], E_t[1], 1000.0, 0.0)
         
-        Q=axis_manip.init_scatt_wavevector_to_scalar_Q(k_j[0], k_j[1],
-                                                       k[0], k[1],
-                                                       angle, angle_err2)
+        Q = axis_manip.init_scatt_wavevector_to_scalar_Q(k_j[0], k_j[1],
+                                                         k[0], k[1],
+                                                         angle, angle_err2)
 
         so2 = SOM.SO(2)
         so2.id = so.id
@@ -114,39 +114,39 @@ def create_E_vs_Q(obj,E,k,px_separate=True,*args,**kwargs):
         so2.var_y = nessi_list.NessiList(y2d_len)
 
         for m in range(y_len):
-            so2.y[m*(y_len+1)] = yval[m]
-            so2.var_y[m*(y_len+1)] = yerr2[m]
+            so2.y[m * (y_len + 1)] = yval[m]
+            so2.var_y[m * (y_len + 1)] = yerr2[m]
         
         so2.axis[0].val = Q[0]
         so2.axis[1].val = E_t[0]
 
-        if so.axis[0].var != None:
+        if so.axis[0].var is not None:
             so2.axis[0].var = Q[1]
             so2.axis[1].var = E_t[1]
         # Do nothing
         else:
             pass
 
-        hlr_utils.result_insert(result,res_descr,so2,None,"all")
+        hlr_utils.result_insert(result, res_descr, so2, None, "all")
 
     return result
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
 
     import hlr_test
     import SOM
 
-    som1=hlr_test.generate_som()
+    som1 = hlr_test.generate_som()
     som1.setAllAxisUnits(["Angstroms"])
     som1.attr_list.instrument = SOM.ASG_Instrument()
 
-    E = (1,1)
-    k = (1,1)
+    E_val = (1, 1)
+    k_val = (1, 1)
     
     print "********** SOM1"
-    print "* ",som1[0]
-    print "* ",som1[1]
+    print "* ", som1[0]
+    print "* ", som1[1]
 
     print "********** create_E_vs_Q"
-    print "* som: ",create_E_vs_Q(som1,E,k)
+    print "* som: ", create_E_vs_Q(som1, E_val, k_val)
