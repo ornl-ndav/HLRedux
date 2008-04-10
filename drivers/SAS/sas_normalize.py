86f14ee70fd622dc7895880b68ff511231d6a106
zp1 <zolnierczukp@ornl.gov>
2008-04-10 15:33:52 +0000

    further updates

--- drivers/SAS/sas_normalize.py
+++ drivers/SAS/sas_normalize.py
@@ -1,47 +1,81 @@
 #!/usr/bin/env python
 
-import os, sys, math
+import os
 from   time import ctime
+
 import DST
-import nessi_list
 import common_lib
 import dr_lib
 import hlr_utils
 
 
-def normalize_to_monitor(fileName,
-                         time_offset_detector,time_offset_monitor,
-                         lambda_min,lambda_max):
+def normalize_to_monitor(fileName,time_offset_detector,time_offset_monitor,**kwargs):
     """Work in progress
     TODO - write documentation
-    function parameters:
-      fileName   - NeXus data file name
-      time_offset_detector - time offset for 2D detector
-      time_offset_monitor  - time offset for BM detector
-      lambda_min           - min wavelength for BM normalization
-      lambda_max           - max wavelength for BM normalization
+         - use hlr_config
+         - the function should be probably called
+           normalize_sas_data(datalist,conf, **kwargs)
+
+    @param fileName: NeXus data file name
+    @type  fileName: C{string}
+
+    @param time_offset_detector: time offset for area detector
+    @type  time_offset_detector: C{tuple} (val,val_err)
+
+    @param time_offset_monitor: time offset for beam monitor
+    @type  time_offset_monitorr: C{tuple} (val,val_err)
+
+    @param kwargs: A list of keyword arguments that the function accepts:
+    
+    @keyword mon_eff: if True, 1/v efficiency correction will be applied
+    to beam monitor spectrum
+    @type mon_eff: C{bool}
+
+    @keyword verbose: 
+    @type verbose: C{bool}
+
+    @keyword debug: 
+    @type verbose: C{bool}
+  
+    @return: Object that has undergone all requested processing steps
+    @rtype: C{SOM.SOM}
+
+    
     """
     # -------------------------------------------------------------------
+    #TODO: move these constants to config/options
     so_axis    = "time_of_flight"          # main SOM axis
     data_bank  = "/entry/bank1,1"          # NeXus path for 2D data
     bmon_bank  = "/entry/monitor1,1"       # NeXus path for beam monitor
     # -------------------------------------------------------------------
-    print ctime(),'normalize_to_monitor:',os.path.basename(fileName)
+    # process keyword arguments
+    mon_eff    = False
+    verbose    = False
+    debug      = False
+    if kwargs.has_key('mon_eff'): mon_eff = kwargs['mon_eff']
+    if kwargs.has_key('verbose'): verbose = kwargs['verbose']
+    if kwargs.has_key('debug'  ): debug   = kwargs['debug']
+    # -------------------------------------------------------------------
+    if verbose:
+        print ctime(),'normalize_to_monitor:',os.path.basename(fileName)
     config            = hlr_utils.Configure()
     config.data       = fileName
     config.data_paths = hlr_utils.create_data_paths(data_bank)
     config.bmon_path  = hlr_utils.create_data_paths(bmon_bank)
     data_dst   = DST.getInstance("application/x-NeXus", config.data)
     # -------------------------------------------------------------------
-    print ctime(),"normalize_to_monitor: getting spectrum object ..."
+    if verbose:
+        print ctime(),"normalize_to_monitor: getting spectrum object"
     s1 = data_dst.getSOM(config.data_paths, so_axis)
     m1 = data_dst.getSOM(config.bmon_path , so_axis)
     # -------------------------------------------------------------------
-    print ctime(),"normalize_to_monitor: fixing bin contents ..."
+    if verbose:
+        print ctime(),"normalize_to_monitor: fixing bin contents"
     s2 = dr_lib.fix_bin_contents(s1)
     m2 = dr_lib.fix_bin_contents(m1)
     # -------------------------------------------------------------------
-    print ctime(),"normalize_to_monitor: converting to scalar wavelength ..."
+    if verbose:
+        print ctime(),"normalize_to_monitor: converting to scalar wavelength"
     s3 = common_lib.tof_to_wavelength_lin_time_zero(s2,units="microsecond",
                                      time_zero_offset=time_offset_detector,
                                      inst_param='total'  )
@@ -49,20 +83,26 @@ def normalize_to_monitor(fileName,
                                      time_zero_offset=time_offset_monitor,
                                      inst_param='primary')
     # -------------------------------------------------------------------
-    print ctime(),"normalize_to_monitor: correcting for monitor efficiency ..."
-    m4 = dr_lib.feff_correct_mon(m3)
-    # -------------------------------------------------------------------
-    print ctime(),"normalize_to_monitor: making dimensionless monitor  ..."
-    m5 = dr_lib.dimensionless_mon(m4, lambda_min, lambda_max)
-    # -------------------------------------------------------------------
-    print ctime(),"normalize_to_monitor: rebinning bmon ..."
-    m6 = dr_lib.rebin_monitor(m5, s3)
-    # -------------------------------------------------------------------
-    print ctime(),"normalize_to_monitor: normalizing ..."
-    s4 = common_lib.div_ncerr(s3,m6)
-    # -------------------------------------------------------------------
-    print ctime(),"normalize_to_monitor: done"
-    return m5,s3,s4
+    if mon_eff:
+        if verbose:
+            print ctime(),"normalize_to_monitor: correcting for monitor efficiency"
+        m4 = dr_lib.feff_correct_mon(m3)
+    else:
+        m4 = m3
 
+    # -------------------------------------------------------------------
+    if verbose:
+        print ctime(),"normalize_to_monitor: rebinning beam monitor"
+    m5 = dr_lib.rebin_monitor(m4, s3)
+    # -------------------------------------------------------------------
+    if verbose:
+        print ctime(),"normalize_to_monitor: normalizing data to monitor"
+    s4 = common_lib.div_ncerr(s3,m5)
+    # -------------------------------------------------------------------
+    if verbose:
+        print ctime(),"normalize_to_monitor: done."
+    if debug:
+        return m4,s3,s4
+    return s4
 
 
--- drivers/SAS/sas_reduction.py
+++ drivers/SAS/sas_reduction.py
@@ -4,28 +4,25 @@ import os, sys
 import hlr_utils
 from   sas_normalize import *
 
+VERSION="0.01"
+
 def getXY(s):
     x=list(s[0].axis[0].val[:-1])        
     y=list(s[0].y[:]            )
     return x,y
 
-def nexusFilePath(topDir,run):
-    return "%s/%s/NeXus/SANS_%s.nxs"    % (topDir,run,run)
+def nexusFilePath(archiveDir,proposal,run):
+    return "%s/%s/1/%s/NeXus/SANS_%s.nxs"    % (archiveDir,proposal,run,run)
 
 
 if __name__ == "__main__":
     """TODO: Cleanup this mess below"""
+    import optparse
     from   pylab import *
+    bragg_edge  = 4.04          # Beryllium Bragg edge - for display purposes
+    nexusArchive= "/LENS/SANS"  # LENS NeXus archive directory
 
-    bragg_edge = 4.04                      # Beryllium Bragg edge - for display purposes
-    nexusDir   ="/LENS/SANS/2008_01_COM/1" # where to find nexus files
-    run_num    =  48                       # run number
-    tof_2d     = -800.0                    # TOF offset for 2D 
-    tof_bm     =  800.0                    # TOF offset for BM
-    lambda_min =    4.0                    # min wavelength for BM normalization
-    lambda_max =   20.0                    # max wavelength for BM normalization
-    
-
+    #FIXME: hard-coded axis ranges and binning
     lambda_axis = hlr_utils.Axis(3.00,23.00,0.10,
                                  units='Angstrom',
                                  title='lambda', bintype='lin') # lambda axis [Angstroms]
@@ -34,75 +31,102 @@ if __name__ == "__main__":
                                  units='1/Angstrom',
                                  title='q', bintype='log')      # q axis [1/Angstroms]
 
-    if len(sys.argv)>1: run_num = sys.argv[1]             
-    if len(sys.argv)>2: tof_2d  = int(sys.argv[2])
-    if len(sys.argv)>3: tof_bm  = int(sys.argv[3])
-
-    nexusFile = nexusFilePath(nexusDir,run_num)
-
-    print 'Arguments: ',sys.argv
-    print len(lambda_axis.toNessiList()),lambda_axis.toNessiList()
-    print len(q_axis.toNessiList()),q_axis.toNessiList()
-    print nexusFile, os.path.exists(nexusFile)
-
+    # parse options
+    argv0   = os.path.basename(sys.argv[0])
+    usage   = "%s [options] <run_number>" % argv0
+    version = "%s %s" % (argv0,VERSION)
+    opt = optparse.OptionParser(usage=usage,version=version)
+    opt.add_option('--time-offset-2d','-d', dest='tof_2d', default=-800,
+                   help='set area detector TOF offset, default:-800')
+    opt.add_option('--time-offset-bm','-m', dest='tof_bm', default=800,
+                   help='set beam monitor detector TOF offset, default:800')
+    opt.add_option('--q-plot'  ,'-q', dest='qplot'   , default=False, action='store_true',
+                   help='plot q')
+    opt.add_option('--mon-eff' ,'-e', dest='mon_eff' , default=False, action='store_true',
+                   help='correct for 1/v monitor efficiency')
+    opt.add_option('--proposal','-p', dest='proposal', default='2008_01_COM',
+                   help='set SANS proposal id, default:2008_01_COM')
+    options, arguments = opt.parse_args()
     
+    tof_2d  = int(options.tof_2d) # TOF offset for 2D (area detector) 
+    tof_bm  = int(options.tof_bm) # TOF offset for BM
+
+    # for now only single run allowed
+    if len(arguments)!=1:
+        print "%s: exactly one run number required" % argv0
+        opt.print_help()
+        sys.exit(-1)
+
+    run = arguments[0]
+    nexusFile = nexusFilePath(nexusArchive,options.proposal,run)
+
+    print 'NeXus file',nexusFile, 'exists?',os.path.exists(nexusFile)
+    print 'l axis:',lambda_axis.toNessiList()
+    print 'q axis:',q_axis.toNessiList()
+    print 'tof_offset_2d=',tof_2d
+    print 'tof_offset_bm=',tof_bm
+
+    # Result of normalize_to_monitor is a tuple (m1,s1,s2)
     # m1 - beam monitor spectrum (wavelength)
-    # s1 - 'raw' area detector spectrum (wavelength, pixel-by-pixel)
+    # s1 - area detector spectrum (wavelength, pixel-by-pixel)
     # s2 - normalized area detector spectrum (wavelength, pixel-by-pixel)
+    # TODO: add error associated with tof_2d, tof_bm (currently 0.0)
     m1,s1,s2 = normalize_to_monitor(nexusFile,
                                     time_offset_detector=(tof_2d,0.0),
                                     time_offset_monitor =(tof_bm,0.0),
-                                    lambda_min=(lambda_min,0.0),
-                                    lambda_max=(lambda_max,0.0))
-
-    # -------------------------------------------------------------------
-    print ctime(),"summing beam monitor ..."
-    bm = common_lib.rebin_axis_1D(m1,axis_out=lambda_axis.toNessiList()) # summed BM wavelength spectrum
-    print ctime(),"summing area detector ..."
-    sl = dr_lib.sum_all_spectra(s1,rebin_axis=lambda_axis.toNessiList()) # summed 2D wavelength spectrum
-    print ctime(),"summing area detector (normalized)..."
-    sn = dr_lib.sum_all_spectra(s2,rebin_axis=lambda_axis.toNessiList()) # summed 2D wavelength normalized spec
-    print ctime(),"converting to scalar q..."
-    s3 = common_lib.wavelength_to_scalar_Q(s2) 
-    print ctime(),"summing q spectrum ..."
-    sq = dr_lib.sum_all_spectra(s3,rebin_axis=q_axis.toNessiList())
+                                    mon_eff=options.mon_eff,
+                                    verbose=True, debug=True)
     
-
-
+    # -------------------------------------------------------------------
+    print ctime(),"rebinning beam monitor wavelength spectrum"
+    bm = common_lib.rebin_axis_1D(m1,axis_out=lambda_axis.toNessiList()) 
+    print ctime(),"summing area detector wavelength spectrum"
+    sl = dr_lib.sum_all_spectra(s1,rebin_axis=lambda_axis.toNessiList()) 
+    print ctime(),"summing area detector wavelength spectrum (normalized)"
+    sn = dr_lib.sum_all_spectra(s2,rebin_axis=lambda_axis.toNessiList()) 
+    if options.qplot:
+        print ctime(),"converting wavelength to scalar q"
+        s3 = common_lib.wavelength_to_scalar_Q(s2) 
+        print ctime(),"summing q spectrum"
+        sq = dr_lib.sum_all_spectra(s3,rebin_axis=q_axis.toNessiList())
+    else:
+        sq = None
+    
     # -------------------------------------------------------------------
     # lets' plot
-    print ctime(),"plotting ..."
-
-
+    print ctime(),"plotting results"
+    
     xm,ym = getXY(bm)
     xs,ys = getXY(sl)
     xn,yn = getXY(sn)
-    xq,yq = getXY(sq)
-
+   
+    
     f1  = figure(1)
     subplot(311)
     semilogy(xm,ym)
     axvline(bragg_edge)
-    ylabel('Intensity BM')
+    ylabel('Intensity (BM)')
     title(os.path.basename(nexusFile))
-
+    
     
     subplot(312)
     semilogy(xs,ys)
     axvline(bragg_edge)
-    ylabel('Intensity Area')
+    ylabel('Intensity (2D)')
     
     subplot(313)
     semilogy(xn,yn)
     axvline(bragg_edge)
     xlabel(r'$\lambda$ [$\AA$]')
-    ylabel('Intensity Area/BM')
+    ylabel('Intensity (2D/BM)')
+    
 
-
-    f2  = figure(2)
-    loglog(xq,yq,'o')
-    xlabel(r'q [1/$\AA$]')
-    ylabel('Intensity')
-    title(os.path.basename(nexusFile))
-
+    if sq:
+        xq,yq = getXY(sq)
+        f2  = figure(2)
+        loglog(xq,yq,'o')
+        xlabel(r'q [1/$\AA$]')
+        ylabel('Intensity')
+        title(os.path.basename(nexusFile))
+    
     show()                #
