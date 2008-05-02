6033715e33d325114cde7be3122d9aaecb066714
Michael Reuter <reuterma@ornl.gov>
2008-05-02 15:50:36 +0000

    Making data cleaning function handle 1D data only and adding functionality. This refs #728.

--- drplot/hlr_plot_utils.py
+++ drplot/hlr_plot_utils.py
@@ -23,15 +23,61 @@
 
 import numpy
 
-def clean_data(axis, x, y, var_y=None, var_x=None):
+def clean_1D_data(action, axis="y", *args):
     """
     This function cleans data for plotting.
 
-    @param axis: The particular axis to clean. This is either I{x} or I{y}.
-    @type axis:
+    @param action: The type of cleaning to perform. The available options are:
+                   - nonzero: Remove all zeros from the data array. This will
+                              reduce the size of all arrays
+    @type action: C{string}
 
+    @param axis: The particular axis to clean. This is either I{x} or I{y}.
+                 The default value is I{y}.
+    @type axis: C{string}
+
+    @param args: The list of data arrays to clean up. The arrays should be
+                 listed in the following order:
+                   1. Independent axis
+                   2. Dependent axis
+                   3. Uncertainty on the dependent axis
+                   4. Uncertainty on the independent axis
+    @type args: C{NumPy} arrays
+    
 
     @return: The cleaned arrays. The order is I{(x, y, var_y, var_x)}.
     @rtype: C{tuple} of C{NumPy} arrays
+
+
+    @raise RuntimeError: The requested action is not recognized
+    @raise RuntimeError: The requested axis is not recognized
     """
+    x = args[0]
+    y = args[1]
+    try:
+        var_y = args[2]
+    except IndexError:
+        var_y = None
+    try:
+        var_x = args[3]
+    except IndexError:
+        var_x = None        
+
+    if action == "nonzero":
+        if axis == "y":
+            indicies = y.nonzero()
+        elif axis == "x":
+            indicies = x.nonzero()
+        else:
+            raise RuntimeError("Cleaning only understands axis x or y and "\
+                               +"not: %s" % axis) 
+        x = x[indicies]
+        y = y[indicies]
+        if var_y is not None:
+            var_y = var_y[indicies]
+        if var_x is not None:
+            var_x = var_x[indicies]                
+    else:
+        raise RuntimeError("Do not understand cleaning action: %s" % action)
+    
     return (x, y, var_y, var_x)
