585462dc7c1d2b1297f568d7d9cf7652df6bcde3
Michael Reuter <reuterma@ornl.gov>
2008-05-09 19:47:09 +0000

    Adding function to install system. This refs #736.

--- dr_lib/__init__.py
+++ dr_lib/__init__.py
@@ -55,6 +55,7 @@ from hlr_rebin_monitor import *
 from hlr_subtract_bkg_from_data import *
 from hlr_subtract_time_indep_bkg import *
 from hlr_sum_all_spectra import *
+from hlr_sum_by_rebin_frac import *
 from hlr_zero_bins import *
 from hlr_zero_spectra import *
 
