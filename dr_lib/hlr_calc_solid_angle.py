6f65296393a3146e9abb6b120124d902c8c71209
Michael Reuter <reuterma@ornl.gov>
2008-06-04 13:22:13 +0000

    Stubbing out solid angle function. This refs #760.

---
+++ dr_lib/hlr_calc_solid_angle.py
@@ -0,0 +1,47 @@
+#                  High-Level Reduction Functions
+#           A part of the SNS Analysis Software Suite.
+#
+#                  Spallation Neutron Source
+#          Oak Ridge National Laboratory, Oak Ridge TN.
+#
+#
+#                             NOTICE
+#
+# For this software and its associated documentation, permission is granted
+# to reproduce, prepare derivative works, and distribute copies to the public
+# for any purpose and without fee.
+#
+# This material was prepared as an account of work sponsored by an agency of
+# the United States Government.  Neither the United States Government nor the
+# United States Department of Energy, nor any of their employees, makes any
+# warranty, express or implied, or assumes any legal liability or
+# responsibility for the accuracy, completeness, or usefulness of any
+# information, apparatus, product, or process disclosed, or represents that
+# its use would not infringe privately owned rights.
+#
+
+# $Id$
+
+def calc_solid_angle(inst, pix, **kwargs):
+    """
+    This function calculates the solid angle of a given pixel by taking the
+    area of the pixel and dividing it by the square of the pathlength.
+
+    @param inst: The object containing the geometry information.
+    @type inst: C{Instrument} or C{CompositeInstrument}
+
+    @param pix: The pixel to calculate the solid angle for.
+    @type pix: C{SOM.SO}
+
+    @param kwargs: A list of keyword arguments that the function accepts:
+
+    @keyword pathtype: The pathlength type from which to calculate the solid
+    angle. The possible values are I{total} and I{primary}. The default is
+    I{total}.
+    @type pathtype: C{string}
+    """
+    import hlr_utils
+
+    return 0.0
+    
+    
