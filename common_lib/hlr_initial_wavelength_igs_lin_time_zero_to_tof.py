702d94532bdc081b7c1b27be02bff93c54a980dc
Michael Reuter <reuterma@ornl.gov>
2008-06-26 20:15:29 +0000

    Adding more functionality and testing code. This refs #765.

--- common_lib/hlr_initial_wavelength_igs_lin_time_zero_to_tof.py
+++ common_lib/hlr_initial_wavelength_igs_lin_time_zero_to_tof.py
@@ -80,6 +80,203 @@ def initial_wavelength_igs_lin_time_zero_to_tof(obj, **kwargs):
     (result, res_descr) = hlr_utils.empty_result(obj)
     o_descr = hlr_utils.get_descr(obj)
 
+    # Setup keyword arguments
+    try:
+        lambda_f = kwargs["lambda_f"]
+    except KeyError:
+        lambda_f = None
+
+    try:
+        time_zero_slope = kwargs["time_zero_slope"]
+    except KeyError:
+        time_zero_slope = None
+
+    # Current constants for Time Zero Slope
+    TIME_ZERO_SLOPE = (float(0.0), float(0.0))
+
+    try:
+        time_zero_offset = kwargs["time_zero_offset"]
+    except KeyError:
+        time_zero_offset = None        
+
+    # Current constants for Time Zero Offset
+    TIME_ZERO_OFFSET = (float(0.0), float(0.0))
+
+    try:
+        dist_source_sample = kwargs["dist_source_sample"]
+    except KeyError:
+        dist_source_sample = None
+
+    try:
+        dist_sample_detector = kwargs["dist_sample_detector"]
+    except KeyError:
+        dist_sample_detector = None
+
+    try:
+        lojac = kwargs["lojac"]
+    except KeyError:
+        lojac = hlr_utils.check_lojac(obj)
+
+    try:
+        units = kwargs["units"]
+    except KeyError:
+        units = "Angstroms"
+
+    # Primary axis for transformation. If a SO is passed, the function, will
+    # assume the axis for transformation is at the 0 position
+    if o_descr == "SOM":
+        axis = hlr_utils.one_d_units(obj, units)
+    else:
+        axis = 0
+
+    result = hlr_utils.copy_som_attr(result, res_descr, obj, o_descr)
+    if res_descr == "SOM":
+        result = hlr_utils.force_units(result, "Microseconds", axis)
+        result.setAxisLabel(axis, "time-of-flight")
+        result.setYUnits("Counts/uS")
+        result.setYLabel("Intensity")
+    else:
+        pass
+
+    # Where to get instrument information
+    if dist_source_sample is None or dist_sample_detector is None:
+        if o_descr == "SOM":
+            try:
+                obj.attr_list.instrument.get_primary()
+                inst = obj.attr_list.instrument
+            except RuntimeError:
+                raise RuntimeError("A detector was not provided!")
+        else:
+            if dist_source_sample is None and dist_sample_detector is None:
+                raise RuntimeError("If a SOM is not passed, the "\
+                                   +"source-sample and sample-detector "\
+                                   +"distances must be provided.")
+            elif dist_source_sample is None:
+                raise RuntimeError("If a SOM is not passed, the "\
+                                   +"source-sample distance must be provided.")
+            elif dist_sample_detector is None:
+                raise RuntimeError("If a SOM is not passed, the "\
+                                   +"sample-detector distance must be "\
+                                   +"provided.")
+            else:
+                raise RuntimeError("If you get here, see Steve Miller for "\
+                                   +"your mug.")
+    else:
+        pass
+        
+    if lambda_f is not None:
+        l_descr = hlr_utils.get_descr(lambda_f)
+    else:
+        if o_descr == "SOM":
+            try:
+                som_l_f = obj.attr_list["Wavelength_final"]
+            except KeyError:
+                raise RuntimeError("Please provide a final wavelength "\
+                                   +"parameter either via the function call "\
+                                   +"or the SOM")
+        else:
+            raise RuntimeError("You need to provide a final wavelength")
+            
+
+    if time_zero_slope is not None:
+        t_0_slope_descr = hlr_utils.get_descr(time_zero_slope)
+    else:
+        if o_descr == "SOM":
+            try:
+                t_0_slope = obj.attr_list["Time_zero_slope"][0]
+                t_0_slope_err2 = obj.attr_list["Time_zero_slope"][1]
+            except KeyError:
+                t_0_slope = TIME_ZERO_SLOPE[0]
+                t_0_slope_err2 = TIME_ZERO_SLOPE[1]
+        else:
+            t_0_slope = TIME_ZERO_SLOPE[0]
+            t_0_slope_err2 = TIME_ZERO_SLOPE[1]
+
+
+    if time_zero_offset is not None:
+        t_0_offset_descr = hlr_utils.get_descr(time_zero_offset)
+    else:
+        if o_descr == "SOM":
+            try:
+                t_0_offset = obj.attr_list["Time_zero_offset"][0]
+                t_0_offset_err2 = obj.attr_list["Time_zero_offset"][1]
+            except KeyError:
+                t_0_offset = TIME_ZERO_OFFSET[0]
+                t_0_offset_err2 = TIME_ZERO_OFFSET[1]
+        else:
+            t_0_offset = TIME_ZERO_OFFSET[0]
+            t_0_offset_err2 = TIME_ZERO_OFFSET[1]
+            
+    if dist_source_sample is not None:
+        ls_descr = hlr_utils.get_descr(dist_source_sample)
+    # Do nothing, go on
+    else:
+        pass
+
+    if dist_sample_detector is not None:
+        ld_descr = hlr_utils.get_descr(dist_sample_detector)
+    # Do nothing, go on
+    else:
+        pass
+
+    # iterate through the values
+    import axis_manip
+    if lojac:
+        import utils
+
+    len_obj = hlr_utils.get_length(obj)
+
+    MNEUT_OVER_H = 1.0 / 0.003956034
+
+    for i in xrange(len_obj):
+        val = hlr_utils.get_value(obj, i, o_descr, "x", axis)
+        err2 = hlr_utils.get_err2(obj, i, o_descr, "x", axis)
+
+        map_so = hlr_utils.get_map_so(obj, None, i)
+
 
     return result
+
+if __name__ == "__main__":
+    import hlr_test
+    import SOM
+
+    l_f_i = (7.0, 0.1)    
+    d_ss = (15.0, 0.1)
+    d_sd = (1.0, 0.05)
+    t_0_s = (10.0, 1.0)
+    t_0_o = (0.1, 0.001)
+
+    som1 = hlr_test.generate_som()
+    som1.setAllAxisUnits(["Angstroms"])
+    som1.attr_list["Wavelength_final"] = l_f_i
+    som1.attr_list["Time_zero_slope"] = t_0_s
+    som1.attr_list["Time_zero_offset"] = t_0_o
+    som1.attr_list.instrument = SOM.ASG_Instrument()
+    
+    print "********** SOM1"
+    print "* ", som1[0]
+    print "* ", som1[1]
+
+    print "********** initial_wavelength_igs_lin_time_zero_to_tof"
+    print "* som  :", initial_wavelength_igs_lin_time_zero_to_tof(
+        som1,
+        run_filter=False)
+    print "* so   :", initial_wavelength_igs_lin_time_zero_to_tof(
+        som1[0],
+        lambda_f=l_f_i,
+        time_zero_slope=t_0_s,
+        time_zero_offset=t_0_o,
+        dist_source_sample=d_ss,
+        dist_sample_detector=d_sd,
+        run_filter=False)
+    print "* scal :", initial_wavelength_igs_lin_time_zero_to_tof(
+        [1, 1],
+        lambda_f=l_f_i,
+        time_zero_slope=t_0_s,
+        time_zero_offset=t_0_o,
+        dist_source_sample=d_ss,
+        dist_sample_detector=d_sd,
+        run_filter=False)
+
     
