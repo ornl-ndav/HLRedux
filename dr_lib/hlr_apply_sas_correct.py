ac3bb589cef7c334aea18629a4d6bb314da60bb1
Michael Reuter <reuterma@ornl.gov>
2008-07-07 15:11:29 +0000

    Filling in functionality for corrections. This refs #777.

--- dr_lib/hlr_apply_sas_correct.py
+++ dr_lib/hlr_apply_sas_correct.py
@@ -24,6 +24,68 @@
 
 def apply_sas_correct(obj):
     """
-    
+    This function applies the following corrections to SAS TOF data:
+      - Multiply counts by pixel radius
+      - Multiply counts by TOF^2 (uses bin centers)
+
+    @param obj: The data to apply the corrections to
+    @type obj: C{SOM.SOM} or C{SOM.SO}
+
+
+    @return: The data after corrections have been applied
+    @rtype: C{SOM.SOM} or C{SOM.SO}
+
+
+    @raise TypeError: The object being rebinned is not a C{SOM} or a C{SO}    
     """
-    return obj
+    # import the helper functions
+    import hlr_utils
+
+    # set up for working through data
+    
+    o_descr = hlr_utils.get_descr(obj)
+
+    if o_descr == "number" or o_descr == "list":
+        raise TypeError("Do not know how to handle given type: %s" % \
+                        o_descr)
+    else:
+        pass
+
+    if o_descr == "SOM":
+        inst = obj.attr_list.instrument
+    else:
+        inst = None
+    
+    (result, res_descr) = hlr_utils.empty_result(obj)
+    
+    result = hlr_utils.copy_som_attr(result, res_descr, obj, o_descr)
+
+    # iterate through the values
+    import array_manip
+    import utils
+
+    import math
+
+    len_obj = hlr_utils.get_length(obj)
+    for i in xrange(len_obj):
+        val = hlr_utils.get_value(obj, i, o_descr, "y")
+        err2 = hlr_utils.get_err2(obj, i, o_descr, "y")
+        axis = hlr_utils.get_value(obj, i, o_descr, "x", axis_pos)
+        
+        (bin_center, bin_center_err2) = utils.calc_bin_centers(axis)
+        
+        map_so = hlr_utils.get_map_so(obj, None, i)
+
+        xpos = hlr_utils.get_parameter("x-offset", map_so, inst)
+        ypos = hlr_utils.get_parameter("y-offset", map_so, inst)
+        
+        radius = math.sqrt(xpos * xpos + ypos * ypos)
+
+        constant = array_manip.mult_ncerr(bin_center, bin_center_err2,
+                                          radius, 0.0) 
+
+        value = array_manip.mult_ncerr(val, err2, constant[0], constant[1])
+        
+        hlr_utils.result_insert(result, res_descr, value, map_so, "y")
+    
+    return result
