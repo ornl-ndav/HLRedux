802214581a3627722e6e84f03cedd3712cdde1a0
Michael Reuter <reuterma@ornl.gov>
2008-07-21 17:49:52 +0000

    Switching over to new correction formula. This refs #777.

--- dr_lib/hlr_apply_sas_correct.py
+++ dr_lib/hlr_apply_sas_correct.py
@@ -72,31 +72,17 @@ def apply_sas_correct(obj):
         err2 = hlr_utils.get_err2(obj, i, o_descr, "y")
         axis = hlr_utils.get_value(obj, i, o_descr, "x", 0)
         
-        #(bin_center, bin_center_err2) = utils.calc_bin_centers(axis)
-
-        #(bin_center2, bin_center2_err2) = array_manip.mult_ncerr(\
-        #    bin_center, bin_center_err2, bin_center, bin_center_err2)
-        
         map_so = hlr_utils.get_map_so(obj, None, i)
 
-        xpos = hlr_utils.get_parameter("x-offset", map_so, inst)
-        ypos = hlr_utils.get_parameter("y-offset", map_so, inst)
+        polar = hlr_utils.get_parameter("polar", map_so, inst)
+
+        sin_pol = math.sin(polar[0])
+        cos_pol = math.cos(polar[0])
+        tan_pol = math.tan(polar[0])
         
-        radius = math.sqrt(xpos * xpos + ypos * ypos)
+        scale = (sin_pol * cos_pol) / (1.0 + (tan_pol * tan_pol))
 
-        #constant = array_manip.mult_ncerr(bin_center, bin_center_err2,
-        #                                  radius, 0.0)
-
-        #constant = array_manip.mult_ncerr(bin_center2, bin_center2_err2,
-        #                                  radius, 0.0)
-        #constant = array_manip.mult_ncerr(radius, 0.0,
-        #                                  bin_center, bin_center_err2)
-
-        #constant = array_manip.div_ncerr(radius, 0.0,
-        #                                 bin_center2, bin_center2_err2)
-
-        #value = array_manip.mult_ncerr(val, err2, constant[0], constant[1])
-        value = array_manip.mult_ncerr(val, err2, radius, 0.0)
+        value = array_manip.mult_ncerr(val, err2, scale, 0.0)
         
         hlr_utils.result_insert(result, res_descr, value, map_so, "y")
     
