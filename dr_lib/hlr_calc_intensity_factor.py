048c9b04bff488839c881494a6e5bac5f0545be5
Michael Reuter <reuterma@ornl.gov>
2008-07-29 14:01:13 +0000

    Finishing functionality. This resolves #785.

--- dr_lib/hlr_calc_intensity_factor.py
+++ dr_lib/hlr_calc_intensity_factor.py
@@ -40,4 +40,39 @@ def calc_intensity_factor(obj):
     # import the helper functions
     import hlr_utils
 
-    return obj
+    # set up for working through data
+    (result, res_descr) = hlr_utils.empty_result(obj)
+
+    o_descr = hlr_utils.get_descr(obj)
+    result = hlr_utils.copy_som_attr(result, res_descr, obj, o_descr)
+
+    import dr_lib
+    
+    # Get the total sum for the entire data object
+    sum_total = dr_lib.integrate_spectra(obj, total=True, width=True)
+    
+    # iterate through the values
+    len_obj = hlr_utils.get_length(obj)
+    for i in xrange(len_obj):
+        obj1 = hlr_utils.get_value(obj, i, o_descr, "all")
+
+        pix_sum = dr_lib.integrate_axis(obj1, width=True)
+
+        intensity_factor = (len_obj * pix_sum[0]) / sum_total[0].y
+        
+        hlr_utils.result_insert(result, res_descr, (intensity_factor, 0.0),
+                                obj1, "yonly")
+         
+    return result
+
+if __name__ == "__main__":
+    import hlr_test
+
+    som1 = hlr_test.generate_som()
+
+    print "********** SOM1"
+    print "* ", som1[0]
+    print "* ", som1[1]
+
+    print "********** calc_intensity_factor"
+    print "som            :", calc_intensity_factor(som1)
