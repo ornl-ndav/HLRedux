75a13408abe99d9a097d7293466a353e940fdaec
Michael Reuter <reuterma@ornl.gov>
2008-08-22 21:01:11 +0000

    Adding some options and initial implementation code. This refs #823.

--- drivers/GEN/stitch_dave.py
+++ drivers/GEN/stitch_dave.py
@@ -39,6 +39,13 @@ def run(config):
     if config.data is None:
         raise RuntimeError("Need to pass a data filename to the driver "\
                            +"script.")
+
+    dst_type = hlr_utils.file_peeker(config.data[0])
+    if dst_type != "text/Dave2D":
+        raise TypeError("Only Dave2D ASCII files can be handled. Do not "\
+                        +"know how to handle %s." % dst_type)
+
+    
 if __name__ == "__main__":
     import dr_lib
     import hlr_utils
@@ -54,13 +61,27 @@ if __name__ == "__main__":
                                     None, hlr_utils.program_version(), 'error',
                                     " ".join(result))
 
+    # Specify the rescaling constant
+    parser.add_option("-r", "--rescale", dest="rescale", help="Specify a "\
+                      +"constant with which to rescale the final data.")
+
+    # Remove unneeded options
+    parser.remove_option("--inst")
+    parser.remove_option("--facility")
+
     (options, args) = parser.parse_args()
 
     # set up the configuration
     configure = hlr_utils.Configure()
 
-    # Call the configuration setter for InstOptions
+    # Call the configuration setter for BasicOptions
     hlr_utils.BasicConfiguration(parser, configure, options, args)
+
+    # Set the rescaling constant
+    try:
+        configure.rescale = float(options.rescale)
+    except TypeError:
+        configure.rescale = options.rescale
 
     # run the program
     run(configure)
