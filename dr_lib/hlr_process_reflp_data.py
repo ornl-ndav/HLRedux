43d24214697b747f4052f5eb9396ca89b45b6167
Michael Reuter <reuterma@ornl.gov>
2008-08-26 18:28:18 +0000

    Adding monitor normalization code. This refs #816.

--- dr_lib/hlr_process_reflp_data.py
+++ dr_lib/hlr_process_reflp_data.py
@@ -103,6 +103,22 @@ def process_reflp_data(datalist, conf, roi_file, **kwargs):
         # Get TOF bin width
         conf.delta_TOF = d_som1[0].axis[0].val[1] - d_som1[0].axis[0].val[0]
 
+    if conf.mon_norm:
+        if conf.verbose:
+            print "Reading in monitor data from %s file" % dataset_type
+
+        # The [0] is to get the data SOM and ignore the None background SOM
+        dm_som1 = dr_lib.add_files(datalist, Data_Paths=conf.mon_path.toPath(),
+                                   SO_Axis=so_axis,
+                                   dataset_type=dataset_type,
+                                   Verbose=conf.verbose,
+                                   Timer=t)[0]
+        
+        if t is not None:
+            t.getTime(msg="After reading monitor data ")
+    else:
+        dm_som1 = None
+
     # Step 1: Sum all spectra along the low resolution direction
     # Set sorting for REF_L
     if conf.verbose:
@@ -125,38 +141,73 @@ def process_reflp_data(datalist, conf, roi_file, **kwargs):
     del d_som1
 
     # Step N: Convert TOF to wavelength
-    if config.verbose:
+    if conf.verbose:
         print "Converting TOF to wavelength"
 
     if tim is not None:
         tim.getTime(False)
 
     d_som3 = common_lib.tof_to_wavelength(d_som2, units="microsecond")
+    if dm_som1 is not None:
+        dm_som2 = common_lib.tof_to_wavelength(dm_som1, units="microsecond")
+    else:
+        dm_som2 = None
+
+    del dm_som1
 
     if tim is not None:
         tim.getTime(msg="After converting TOF to wavelength ")
 
     del d_som2
 
-    # Step 2: Multiply the spectra by the proton charge
-    if conf.verbose:
-        print "Multiply spectra by proton charge"
+    if conf.mon_norm:
+        dm_som3 = dr_lib.rebin_monitor(dm_som2, d_som3, rtype="frac")
+    else:
+        dm_som3 = None
 
-    pc_tag = dataset_type + "-proton_charge"
-    proton_charge = d_som2.attr_list[pc_tag]
+    del dm_som2
 
-    if t is not None:
-        t.getTime(False)
+    if not conf.mon_norm:
+        # Step 2: Multiply the spectra by the proton charge
+        if conf.verbose:
+            print "Multiply spectra by proton charge"
 
-    d_som3 = common_lib.mult_ncerr(d_som2, (proton_charge.getValue(), 0.0))
+        pc_tag = dataset_type + "-proton_charge"
+        proton_charge = d_som2.attr_list[pc_tag]
 
-    if t is not None:
-        t.getTime(msg="After scaling by proton charge ")
+        if t is not None:
+            t.getTime(False)
 
-    del d_som2
+        d_som3 = common_lib.mult_ncerr(d_som2, (proton_charge.getValue(), 0.0))
+
+        if t is not None:
+            t.getTime(msg="After scaling by proton charge ")
+    else:
+        if conf.verbose:
+            print "Normalize by monitor spectrum"
+
+        if t is not None:
+            t.getTime(False)
+
+        d_som3 = common_lib.div_ncerr(d_som2, dm_som3)
+
+        if t is not None:
+            t.getTime(msg="After monitor normalization ")
+
+    del d_som2, dm_som3
 
     if roi_file is None:
         return d_som3
     else:
         # Step 3: Make one spectrum for normalization dataset
-        return dr_lib.sum_all_spectra(d_som3)
+        # Need to create a final rebinning axis
+        pathlength = d_som3.attr_list.instrument.get_total_path(
+            det_secondary=True)
+        
+        delta_lambda = common_lib.tof_to_wavelength((config.delta_TOF, 0.0),
+                                                    pathlength=pathlength)
+
+        lambda_bins = dr_lib.create_axis_from_data(d_som3,
+                                                   width=delta_lambda[0])
+
+        return dr_lib.sum_by_rebin_frac(d_som3, lambda_bins.toNessiList())
