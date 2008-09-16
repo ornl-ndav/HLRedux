e675ede89b290dc74876f9c6a087e262898a75f2
Michael Reuter <reuterma@ornl.gov>
2008-09-16 12:51:07 +0000

    Finishing out functionality. This resolves #857.

--- dr_lib/hlr_rebin_axis_1D_frac.py
+++ dr_lib/hlr_rebin_axis_1D_frac.py
@@ -49,4 +49,63 @@ def rebin_axis_1D_frac(obj, axis_out):
     # import the helper functions
     import hlr_utils
 
-    return (None, None)
+    # set up for working through data
+    try:
+        axis_out.__type__
+    except AttributeError:
+        raise TypeError("Rebinning axis must be a NessiList!")
+
+    o_descr = hlr_utils.get_descr(obj)
+
+    if o_descr == "number" or o_descr == "list":
+        raise TypeError("Do not know how to handle given type: %s" % \
+                        o_descr)
+    else:
+        pass
+
+    (result1, res1_descr) = hlr_utils.empty_result(obj)
+    result1 = hlr_utils.copy_som_attr(result1, res1_descr, obj, o_descr)
+
+    (result2, res2_descr) = hlr_utils.empty_result(obj)
+    result2 = hlr_utils.copy_som_attr(result2, res2_descr, obj, o_descr)
+
+    # iterate through the values
+    import axis_manip
+
+    len_obj = hlr_utils.get_length(obj)
+    for i in xrange(len_obj):
+        axis_in = hlr_utils.get_value(obj, i, o_descr, "x", 0)
+        val = hlr_utils.get_value(obj, i, o_descr)
+        err2 = hlr_utils.get_err2(obj, i, o_descr)
+
+        value = axis_manip.rebin_axis_1D_frac(axis_in, val, err2, axis_out)
+
+        frac_err = nessi_list.NessiList(len(value[2]))
+
+        xvals = []
+        xvals.append(axis_out)
+
+        map_so = hlr_utils.get_map_so(obj, None, i)
+
+        hlr_utils.result_insert(result1, res1_descr, (value[0], value[1]),
+                                map_so, "all", 0, xvals)
+        hlr_utils.result_insert(result2, res2_descr, (value[2], frac_err),
+                                map_so, "all", 0, xvals)        
+
+    return (result1, result2)
+
+if __name__ == "__main__":
+    import hlr_test
+
+    som1 = hlr_test.generate_som()
+
+    axis = nessi_list.NessiList()
+    axis.extend(0, 2.5, 5)
+
+    print "********** SOM1"
+    print "* ", som1[0]
+    print "* ", som1[1]
+
+    print "********** rebin_axis_1D_frac"
+    print "* rebin som:", rebin_axis_1D_frac(som1, axis)
+    print "* rebin so :", rebin_axis_1D_frac(som1[0], axis)
