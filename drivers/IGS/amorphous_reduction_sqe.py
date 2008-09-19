7ed04839b74a3b77376420b7d11084820ff5910d
Michael Reuter <reuterma@ornl.gov>
2008-09-19 13:50:30 +0000

    Removing pixnorm flag from new spectrum creation call. This refs #822.

--- drivers/IGS/amorphous_reduction_sqe.py
+++ drivers/IGS/amorphous_reduction_sqe.py
@@ -400,7 +400,6 @@ def run(config, tim=None):
                                         "negcos_param_array",
                                         config.ncospol_bins.toNessiList(),
                                         rebin_axis=config.E_bins.toNessiList(),
-                                        pixnorm=True,
                                         prnorm="azimuthal",
                                         y_label="counts",
                                         y_units="counts / (ueV * rads)",
