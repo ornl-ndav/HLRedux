bec05ce2fdea5b848d29841b33beb10dcbee55aa
Michael Reuter <reuterma@ornl.gov>
2008-10-01 17:53:33 +0000

    Adding new function. This resolves #882.

--- common_lib/__init__.py
+++ common_lib/__init__.py
@@ -46,6 +46,7 @@ from hlr_tof_to_initial_wavelength_igs_lin_time_zero import *
 from hlr_tof_to_scalar_Q import *
 from hlr_tof_to_wavelength import *
 from hlr_tof_to_wavelength_lin_time_zero import *
+from hlr_velocity_to_wavelength import *
 from hlr_wavelength_to_d_spacing import *
 from hlr_wavelength_to_energy import *
 from hlr_wavelength_to_scalar_k import *
