3f8ae241dd7a04dc03ae164887842d7247b0890e
Michael Reuter <reuterma@ornl.gov>
2008-12-31 20:33:29 +0000

    Adding functionality. Needs more work. This refs #895.

--- dr_lib/hlr_create_E_vs_Q_dgs.py
+++ dr_lib/hlr_create_E_vs_Q_dgs.py
@@ -40,13 +40,72 @@ def create_E_vs_Q_dgs(som, E_i, Q_final, **kwargs):
 
     @keyword corner_geom: The filename that contains the corner geometry
                           information.
-    @type corner_geom: C{string}    
+    @type corner_geom: C{string}
+
+    @keyword timer: Timing object so the function can perform timing estimates.
+    @type timer: C{sns_timer.DiffTime}    
     """
+    import array_manip
+    import axis_manip
+    import common_lib
+    import hlr_utils
 
     # Check keywords
+    try:
+        t = kwargs["timer"]
+    except KeyError:
+        t = None
+    
     try:
         corner_geom = kwargs["corner_geom"]
     except KeyError:
         corner_geom = ""
+
+    # Convert initial energy to initial wavevector
+    l_i = common_lib.energy_to_wavelength(E_i)
+    k_i = common_lib.wavelength_to_scalar_k(l_i)
+
+    # Since all the data is rebinned to the same energy transfer axis, we can
+    # calculate the final energy axis once
+    E_t = som[0].axis[0].val
+    if som[0].axis[0].var is not None:
+        E_t_err2 = som[0].axis[0].var
+    else:
+        import nessi_list
+        E_t_err2 = nessi_list.NessiList(len(E_t))        
+
+    E_f = array_manip.sub_ncerr(E_i[0], E_i[1], E_t, E_t_err2)
+    
+    # Now we can get the final wavevector
+    l_f = axis_manip.energy_to_wavelength(E_f[0], E_f[1])
+    k_f = axis_manip.wavelength_to_scalar_k(l_f[0], l_f[1])
+
+    # Grab the instrument from the som
+    inst = som.attr_list.instrument
+
+    # Get the corner geoemetry information
+    if t is not None:
+        t.getTime(False)
         
+    corner_angles = hlr_utils.get_corner_geometry(corner_geom)
+
+    if t is not None:
+        t.getTime(msg="After reading in corner geometry information ")
+
+    # Iterate though the data
+    len_som = hlr_utils.get_length(som)
+    for i in xrange(len_som):
+        map_so = hlr_utils.get_map_so(som, None, i)
+
+        yval = hlr_utils.get_value(som, i, "SOM", "y")
+        yerr2 = hlr_utils.get_err2(som, i, "SOM", "y")
+
+        cangles = corner_angles[str(map_so.id)]
+
+        (Q1, Q1_err2) = axis_manip.init_scatt_wavevector_to_scalar_Q(k_i[0],
+                                                                     k_i[1],
+                                                                     k_f[0],
+                                                                     k_f[1],
+                                                           cangles.getPolar(0),
+                                                                     0.0)
     return None
