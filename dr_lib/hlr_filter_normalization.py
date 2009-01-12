acf5a1eaf06e341ab53e5677808c1759bfd0644d
Michael Reuter <reuterma@ornl.gov>
2009-01-12 18:56:19 +0000

    Adding pixel thresholding code. This refs #925.

--- dr_lib/hlr_filter_normalization.py
+++ dr_lib/hlr_filter_normalization.py
@@ -22,26 +22,52 @@
 
 # $Id$
 
-def filter_normalization(obj, threshold):
+def filter_normalization(obj, threshold, config=None):
     """
     This function takes an object with normalization integration information
     and a threshold and creates a mask file containing the pixel IDs that do
     not make it above the threshold.
 
     @param obj: The object containing the normalization information
-    @type obj: C{SOM.SOM} or C{SOM.SO}
+    @type obj: C{SOM.SOM}
 
     @param threshold: The upper bound for masked pixels
     @type threshold: C{float}
 
-    
+    @param config: The object holding the DR configuration
+    @type config: L{hlr_utils.Configure}
+
+
+    @raise TypeError: The incoming object is not a C{SOM}.
     """
     import hlr_utils
 
     o_descr = hlr_utils.get_descr(obj)
+    if o_descr != "SOM":
+        raise TypeError("Only SOMs are allowed in this function!")
+
+    if config is None:
+        # Make mask file name from object information
+        instname = obj.attr_list.inst.get_name()
+        runnum = obj.attr_list["run_number"]
+        outfile = "%s_%s_mask.dat" % (instname, str(runnum))
+    else:
+        # Make mask file name from configuration information
+        outfile = hlr_utils.ext_replace(config.output, config.ext_replacement,
+                                        "dat")
+        outfile = hlr_utils.add_tag(outfile, "ROI")
+
+    ofile = open(outfile, "w")
+
+    import SOM
     
     len_obj = hlr_utils.get_length(obj)
     for i in xrange(len_obj):
         norm = hlr_utils.get_value(obj, i, o_descr)
         if norm < threshold:
             map_so = hlr_utils.get_map_so(obj, None, i)
+            pix_id = SOM.NeXusId.fromString(str(map_so.id)).toJoinedStr()
+
+            print >> ofile, pix_id
+
+    ofile.close()
