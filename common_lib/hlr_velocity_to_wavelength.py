bf45b8c59aa0f9e93e03b302810dabe7eb09068e
Michael Reuter <reuterma@ornl.gov>
2009-03-31 01:43:48 +0000

    Fixing Pylint issues. This resolves #957.

--- common_lib/hlr_velocity_to_wavelength.py
+++ common_lib/hlr_velocity_to_wavelength.py
@@ -73,11 +73,6 @@ def velocity_to_wavelength(obj, **kwargs):
         units = "meters/microseconds"
 
     try:
-        offset = kwargs["offset"]
-    except KeyError:
-        offset = None
-
-    try:
         lojac = kwargs["lojac"]
     except KeyError:
         lojac = hlr_utils.check_lojac(obj)
@@ -99,7 +94,6 @@ def velocity_to_wavelength(obj, **kwargs):
         pass
 
     # iterate through the values
-    import array_manip
     import axis_manip
     if lojac:
         import utils
