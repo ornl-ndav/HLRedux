57c74dea08cb641f5a2cf2b3eff8c9cba22048d8
Michael Reuter <reuterma@ornl.gov>
2009-04-01 20:19:34 +0000

    Adding delta azimuthal scaling. This resolves #987.

--- dr_lib/hlr_create_E_vs_Q_igs.py
+++ dr_lib/hlr_create_E_vs_Q_igs.py
@@ -420,10 +420,14 @@ def create_E_vs_Q_igs(som, *args, **kwargs):
         if configure.dump_pix_contrib or configure.scale_sqe:
             if inst_name == "BSS":
                 dOmega = dr_lib.calc_BSS_solid_angle(map_so, inst)
+                dazi = dr_lib.calc_BSS_delta_azi(map_so, inst)
+
+                sconst = dOmega * dazi
+                
                 (bin_count_new,
                  bin_count_err2) = array_manip.mult_ncerr(bin_count_new,
                                                           bin_count_err2,
-                                                          dOmega, 0.0)
+                                                          sconst, 0.0)
                 
                 (bin_count,
                  bin_count_err2) = array_manip.add_ncerr(bin_count,
