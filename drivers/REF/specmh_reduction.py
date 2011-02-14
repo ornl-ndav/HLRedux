71d14edebca2ba4c87a2ad984451dbbb51b4b7db
Michael Reuter <reuterma@ornl.gov>
2011-02-14 15:04:12 +0000

    Removing old options from driver. This refs #1141.

--- drivers/REF/specmh_reduction.py
+++ drivers/REF/specmh_reduction.py
@@ -357,27 +357,6 @@ if __name__ == "__main__":
     parser.set_defaults(data_paths="/entry/bank1,1")
 
     # Setup REF specific options
-    parser.add_option("", "--ecell", dest="ecell",
-                       help="Specify the empty sample cell file")
-
-    parser.add_option("", "--subtrans-coeff", dest="subtrans_coeff",
-                      nargs=2, type="float", help="Provide the substrate "\
-                      +"transmission coefficients.")
-
-    parser.add_option("", "--substrate-diam", dest="substrate_diam",
-                      type="float",
-                      help="Provide the substrate diameter in cm.")
-
-    parser.add_option("", "--scale-ecell", dest="scale_ecell", type="float",
-                      help="Provide the scaling factor for the empty cell "\
-                      +"subtraction.")
-    parser.set_defaults(scale_ecell=1.0)
-
-    parser.add_option("", "--dump-ecell-rtof", action="store_true",
-                      dest="dump_ecell_rtof",
-                      help="Dump the empty cell spectra after all scaling. "\
-                      +"Creates a *.ertof file.")
-    parser.set_defaults(dump_ecell_rtof=False)
     
     parser.add_option("", "--timing", action="store_true", dest="timing",
                       help="Flag to turn on timing of code")
@@ -390,40 +369,6 @@ if __name__ == "__main__":
 
     # Call the configuration setter for SmhrOptions
     hlr_utils.SmhrConfiguration(parser, configure, options, args)
-
-    # Setup the empty cell file list
-    if hlr_utils.cli_provide_override(configure, "ecell", "--ecell"):
-        configure.ecell = hlr_utils.determine_files(options.ecell,
-                                                    configure.inst,
-                                                    configure.facility)
-
-    # If empty cell subtraction is desired, turn off standard background
-    # subtraction
-    if configure.ecell is not None:
-        configure.no_bkg = True
-        
-    # Setup the substrate transmission coefficients
-    if hlr_utils.cli_provide_override(configure, "subtrans_coeff",
-                                      "--subtrans-coeff"):
-        configure.subtrans_coeff = options.subtrans_coeff
-
-    # Setup the substrate diameter parameter
-    if hlr_utils.cli_provide_override(configure, "substrate_diam",
-                                      "--substrate-diam"):
-        configure.substrate_diam = options.substrate_diam
-
-    # Setup the empty cell scaling parameter
-    if hlr_utils.cli_provide_override(configure, "scale_ecell",
-                                      "--scale_ecell"):
-        configure.scale_ecell = options.scale_ecell        
-
-    # Set the ability to dump the empty cell R(TOF) information
-    if hlr_utils.cli_provide_override(configure, "dump_ecell_rtof",
-                                      "--dump-ecell-rtof"): 
-        configure.dump_ecell_rtof = options.dump_ecell_rtof
-
-    if options.dump_all:
-        configure.dump_ecell_rtof = True
 
     # Setup the timing object
     if options.timing:
