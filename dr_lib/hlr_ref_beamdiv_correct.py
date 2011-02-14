4c1896235324403af075607c31e04e605b277029
Michael Reuter <reuterma@ornl.gov>
2011-02-14 21:35:10 +0000

    Finished acceptance polygon calculation. This refs #1144.

--- dr_lib/hlr_ref_beamdiv_correct.py
+++ dr_lib/hlr_ref_beamdiv_correct.py
@@ -23,8 +23,9 @@
 # $Id$
 
 import math
+import nessi_list
 
-def ref_beamdiv_correct(attrs, pix_id, **kwargs):
+def ref_beamdiv_correct(attrs, pix_id, pathlength, epsilon, **kwargs):
     """
 
     @param attrs: The attribute list of a C{SOM.SOM}
@@ -33,6 +34,13 @@ def ref_beamdiv_correct(attrs, pix_id, **kwargs):
     @param pix_id: The pixel ID for which the correction will be calculated
     @type pix_id: C{tuple}
 
+    @param pathlength: The sample to detector distance.
+    @type pathlength: C{float}
+
+    @param epsilon: The pixel spatial resolution in units of meters
+    @type epsilon: C{float}
+
+    @param kwargs: A list of keyword arguments that the function accepts:
 
     @return: The beam divergence correction to the scattering angle
     @type: float
@@ -44,28 +52,66 @@ def ref_beamdiv_correct(attrs, pix_id, **kwargs):
     if inst_name == "REF_L":
         first_slit_size = "data-slit1_size"
         last_slit_size = "data-slit2_size"
-        last_slit_distot = "data-slit12_distance"
+        last_slit_dist = "data-slit2_distance"
+        slit_dist = "data-slit12_distance"
+
     elif inst_name == "REF_M":
         first_slit_size = "data-slit1_size"
         last_slit_size = "data-slit3_size"
-        last_slit_distot = "data-slit13_distance"
+        last_slit_dist = "data-slit3_distance"
+        slit_dist = "data-slit13_distance"
     else:
         raise RuntimeError("Do not know how to handle instrument %s" \
                            % inst_name)
+
+    # This is currently set to the same number for both REF_L and REF_M
+    if epsilon is None:
+        epsilon = 0.5 * 1.3 * 1.0e-3
     
     gamma_plus = math.atan2(0.5 * (attrs[first_slit_size][0] + \
                                    attrs[last_slit_size][0]),
-                            attrs[last_slit_distot][0])
+                            attrs[slit_dist][0])
     
     gamma_minus = math.atan2(0.5 * (attrs[first_slit_size][0] - \
                                     attrs[last_slit_size][0]),
-                             attrs[last_slit_distot][0])
+                             attrs[slit_dist][0])
     
     half_last_aperture = 0.5 * attrs[last_slit_size][0]
     neg_half_last_aperture = -1.0 * half_last_aperture
 
-    
-    
+    last_slit_to_det = attrs[last_slit_dist][0] + pathlength
 
+    dist_last_aper_det_sin_gamma_plus = last_slit_to_det * math.sin(gamma_plus)
+    dist_last_aper_det_sin_gamma_minus = last_slit_to_det * \
+                                         math.sin(gamma_minus)
+
+    # Set the delta theta coordinates of the acceptance polygon
+    accept_poly_x = nessi_list.NessiList()
+    accept_poly_x.append(-1.0 * gamma_minus)
+    accept_poly_x.append(gamma_plus)
+    accept_poly_x.append(gamma_plus)
+    accept_poly_x.append(gamma_minus)
+    accept_poly_x.append(-1.0 * gamma_plus)
+    accept_poly_x.append(-1.0 * gamma_plus)
+    accept_poly_x.append(accept_poly_x[0])
+
+    # Set the z coordinates of the acceptance polygon
+    accept_poly_y = nessi_list.NessiList()
+    accept_poly_y.append(half_last_aperture - \
+                         dist_last_aper_det_sin_gamma_minus + epsilon)
+    accept_poly_y.append(half_last_aperture + \
+                         dist_last_aper_det_sin_gamma_plus + epsilon)
+    accept_poly_y.append(half_last_aperture + \
+                         dist_last_aper_det_sin_gamma_plus - epsilon)
+    accept_poly_y.append(neg_half_last_aperture + \
+                         dist_last_aper_det_sin_gamma_minus - epsilon)
+    accept_poly_y.append(neg_half_last_aperture - \
+                         dist_last_aper_det_sin_gamma_plus - epsilon)
+    accept_poly_y.append(neg_half_last_aperture - \
+                         dist_last_aper_det_sin_gamma_plus + epsilon)
+    accept_poly_y.append(accept_poly_y[0])
+
+    
+    
     return 0
     
