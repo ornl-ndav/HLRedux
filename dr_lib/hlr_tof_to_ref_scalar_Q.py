70369a35d0dcdbb8fcfe2025793fd32d815b8609
Michael Reuter <reuterma@ornl.gov>
2011-02-23 19:38:27 +0000

    Adding code comment. This resolves #1143.

--- dr_lib/hlr_tof_to_ref_scalar_Q.py
+++ dr_lib/hlr_tof_to_ref_scalar_Q.py
@@ -180,6 +180,8 @@ def tof_to_ref_scalar_Q(obj, **kwargs):
             dangle = dr_lib.ref_beamdiv_correct(obj.attr_list, map_so.id,
                                                 config.det_spat_res,
                                                 config.center_pix)
+            # We subtract due to the inversion of the z coordinates from the
+            # mirror reflection of the beam at the sample.
             pangle = angle - (2.0 * dangle)
         else:
             pangle = angle
